0b086439943172c30326ab2d994a0a02ae243948
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-25 17:58:49 +0900

    [Edit]: Allow classList to manipulate classes
    Message body
    FirstJs_8
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/FirstJS/sample.js
+++ dotInstall/javascriptLesson/FirstJS/sample.js
@@ -2,6 +2,7 @@
 
 function process() {
   document.getElementById('target').textContent = 'すっごく真面目です';
+  document.getElementById('trigger').classList.add('changed');
 }
 
 document.getElementById('trigger').addEventListener('click', process);
