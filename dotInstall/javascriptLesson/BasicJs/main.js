dc0a68ae3ca27e3e6a4200ae64cbb278877f93d7
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-26 04:15:18 +0900

    [Edit]: Conditional branching using statements
    Message body
    BasicJs_13
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/BasicJs/main.js
+++ dotInstall/javascriptLesson/BasicJs/main.js
@@ -1,16 +1,13 @@
 'use strict'
 
-// 以下のようにして比較と評価を行うことができます
-const price = 1200;
+const score = 60;
 
-// 比較演算子
-console.log(price > 1000);    //右辺より大きい
-console.log(price < 1000);    //右辺より小さい
-console.log(price >= 1000);   //右辺以上
-console.log(price <= 1000);   //右辺以下
-console.log(price == 1000);   //右辺と同じ
-console.log(price != 1000);   //右辺と異なる
-
-// 条件分岐を行うために必要な評価の仕方は以下の通りです
-console.log(Boolean(0));
-console.log(Boolean('Hello'));
+if (score >= 80) {
+  console.log('Greate!');
+} else if (score >= 70) {
+  console.log('Good');
+} else if (score <= 60) {
+  console.log('Expect more');
+} else {
+  console.log('OK...');
+}
