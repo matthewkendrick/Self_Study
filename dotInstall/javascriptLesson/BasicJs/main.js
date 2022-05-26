8e5fbb82d3eaf38ce57dba482ae5e4409a15fe4b
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-26 20:04:52 +0900

    [Edit]: Know the Arrow function, which allows you to write short function expressions
    Message body
    BasicJs_24

--- dotInstall/javascriptLesson/BasicJs/main.js
+++ dotInstall/javascriptLesson/BasicJs/main.js
@@ -4,11 +4,10 @@
 //   return a + b + c;
 // }
 
-// 以下のように関数式に変えずとも、基本的には関数宣言で構いませんが、こういった書き方もあるということは知っておきましょう
+// const sum = (a, b, c) => a + b + c;
 
-const sum = function(a, b, c) {
-  return a + b + c;
-}
+// const total = sum(1, 2, 3) + sum(4, 5, 6);
+// console.log(total);
 
-const total = sum(1, 2, 3) + sum(4, 5, 6);
-console.log(total);
+const double = (a) => a * 2
+console.log(double(12));
