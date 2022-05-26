ca9a46327023824b75bfa4fc764511bbb492c373
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-26 17:42:11 +0900

    [Edit]: Learn how to write with return
    Message body
    BasicJs_22
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/BasicJs/main.js
+++ dotInstall/javascriptLesson/BasicJs/main.js
@@ -1,36 +1,14 @@
 'use strict'
 
-// console.log('------------------');
-// console.log('--------Ad--------');
-// console.log('------------------');
-// console.log('Tom is great!');
-// console.log('Bob is great!');
-// console.log('------------------');
-// console.log('--------Ad--------');
-// console.log('------------------');
-// console.log('Steve is great!');
-// console.log('Richard is great!');
-// console.log('------------------');
-// console.log('--------Ad--------');
-// console.log('------------------');
-
-
-// 上記のように何度も使いまわせるような部品は関数としてまとめることで以下のようにスッキリと書くことができます
-
-function showAd(message = 'Ad-second') {
-  //　こちらは値を仮置きして定義しているので"仮引数"と呼びます
-  // また = で繋げることで、以下のようにshowAd()と引数を渡さなかった場合のデフォルト値を設定することができます
-
-  console.log('------------------');
-  console.log(`--------${message}--------`);
-  console.log('------------------');
+function sum(a, b, c) {
+  // console.log(a + b + c);  //これをコメントにすると、それぞれ計算されたsumがreturnに返ってきて、totalだけが出力されるようになります
+  return a + b + c;
+  // return以降の記述は実行されません
 }
 
-showAd('Header Ad')     // 実際に関数を呼び出すときに渡されるため、こちらは"実引数"と呼ばれます
-console.log('Tom is great!');
-console.log('Bob is great!');
-showAd('Ad-first')
-console.log('Steve is great!');
-showAd()
-console.log('Richard is great!');
-showAd('Footer Ad')
+// sum(1, 2, 3);
+// sum(4, 5, 6);
+
+const total = sum(1, 2, 3) + sum(4, 5, 6);
+
+console.log(total);
