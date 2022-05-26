0534d754f4eef7cfc1cbfc6cbcb060978c504677
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-27 08:04:26 +0900

    [Edit]: Know how to access array elements
    Message body
    Object_2
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Object/main.js
+++ dotInstall/javascriptLesson/Object/main.js
@@ -1,10 +1,11 @@
 'use strict'
 
 {
-  // const score1 = 80;
-  // const score2 = 90;
-  // const score3 = 40;
-
   const scores = [80, 90, 40];
+  console.log(scores[1]);
+
+  scores[2] = 44;
   console.log(scores);
+
+  console.log(scores.length);
 }
