f64426b30b4e92f9b9f5e90da8330103812a6fcc
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-26 11:44:53 +0900

    [Edit]: Set default value for arguments
    Message body
    BasicJs_21
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/BasicJs/main.js
+++ dotInstall/javascriptLesson/BasicJs/main.js
@@ -17,16 +17,20 @@
 
 // 上記のように何度も使いまわせるような部品は関数としてまとめることで以下のようにスッキリと書くことができます
 
-function showAd() {
+function showAd(message = 'Ad-second') {
+  //　こちらは値を仮置きして定義しているので"仮引数"と呼びます
+  // また = で繋げることで、以下のようにshowAd()と引数を渡さなかった場合のデフォルト値を設定することができます
+
   console.log('------------------');
-  console.log('--------Ad--------');
+  console.log(`--------${message}--------`);
   console.log('------------------');
 }
 
-showAd()
+showAd('Header Ad')     // 実際に関数を呼び出すときに渡されるため、こちらは"実引数"と呼ばれます
 console.log('Tom is great!');
 console.log('Bob is great!');
-showAd()
+showAd('Ad-first')
 console.log('Steve is great!');
-console.log('Richard is great!');
 showAd()
+console.log('Richard is great!');
+showAd('Footer Ad')
