f0355f9046e2a5a0f1caf69c0f4a9141645b6cd2
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-27 08:18:19 +0900

    [Edit]: Write a loop process using a statement that "for"
    Message body
    Object_3
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Object/main.js
+++ dotInstall/javascriptLesson/Object/main.js
@@ -1,11 +1,16 @@
 'use strict'
 
 {
-  const scores = [80, 90, 40];
-  console.log(scores[1]);
+  const scores = [80, 90, 40, 70];
 
-  scores[2] = 44;
-  console.log(scores);
+  // 文字列と一緒に配列の数値を取り出したい場合は以下のように記述します
+  // console.log(`Score: ${ scores[0] }`);
+  // console.log(`Score: ${ scores[1] }`);
+  // console.log(`Score: ${ scores[2] }`);
 
-  console.log(scores.length);
+  // 以下のようにfor文を用いることで、上記をスッキリと書くことができます
+  // for (let i = 0; i < 3; i++) { // iが０から始まって、順に１ずつ増やす
+  for (let i = 0; i < scores.length; i++) { // 配列の要素に増減があった場合、上記では３の部分を変更しなければならない
+    console.log(`Score ${i}: ${ scores[i] }`); // それに合致する数値を配列からとってきて出力する
+  }
 }
