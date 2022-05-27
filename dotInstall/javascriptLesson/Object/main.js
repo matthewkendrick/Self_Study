dab70c905e4e9c499a8cd0a957c3920190b0d25b
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-27 11:40:02 +0900

    [Edit]: Know about syntax that spread
    Message body
    Object_6
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Object/main.js
+++ dotInstall/javascriptLesson/Object/main.js
@@ -1,15 +1,15 @@
 'use strict'
 
 {
-  const scores = [80, 90, 40, 70];
-  // splice(X, Y, Z, ...)
-  // X : 変化が開始する位置
-  // Y : 削除数
-  // Z : 追加する要素
+  const otherScores = [10, 20];
 
-  scores.splice(1, 1, 40, 50);
+  // スプレッド構文( ... )を使うことによって、配列内で配列を扱えるようになります
+  const scores = [80, 90, 40, 70, ...otherScores];
+  // console.log(scores);
 
-  for (let i = 0; i < scores.length; i++) {
-    console.log(`Score ${i}: ${ scores[i] }`);
+  function sum(a, b) {
+    console.log(a + b);
   }
+
+  sum(...otherScores)
 }
