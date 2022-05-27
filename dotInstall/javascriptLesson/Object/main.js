3d8d618c3e1cd9df80df2f5223db81740353fa4b
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-27 18:26:56 +0900

    [Edit]: Learn how to create objects
    Message body
    Object_11
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Object/main.js
+++ dotInstall/javascriptLesson/Object/main.js
@@ -1,17 +1,11 @@
 'use strict'
 
 {
-  const numbers = [1, 4, 7, 8, 10];
+  // const point = [100, 180];
 
-  // const evenNumbers = numbers.filter(number => {
-  //   if (number % 2 == 0) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // });
-
-  const evenNumbers = numbers.filter(number => number % 2 == 0)
-
-  console.log(evenNumbers);
+  const point = {
+    x: 100,
+    y: 180,
+  };
+  console.log(point);
 }
