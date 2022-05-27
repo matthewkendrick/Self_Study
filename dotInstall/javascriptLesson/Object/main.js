12cc8a61a48dea3a1f8a4741d6082cacfe592ee5
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-27 18:32:01 +0900

    [Edit]: Learn how to create objects
    Message body
    Object_11
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Object/main.js
+++ dotInstall/javascriptLesson/Object/main.js
@@ -7,5 +7,15 @@
     x: 100,
     y: 180,
   };
+
+  point.x = 120;
+  // point[`x`] = 120;
+
+  // console.log(point.x);
+  // console.log(point[`y`]);
+
+  point.z = 90;
+  delete point.y;
+
   console.log(point);
 }
