a076e3f635963c257360d28041871fdbf97fb23c
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-27 10:22:16 +0900

    [Edit]: Modifying array elements
    Message body
    Object_4
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Object/main.js
+++ dotInstall/javascriptLesson/Object/main.js
@@ -2,15 +2,15 @@
 
 {
   const scores = [80, 90, 40, 70];
+  // 以下のような記述によって配列を操作できたりもするので、覚えておくといいでしょう
+  // unshift : 先頭に要素を追加する
+  // push    : 末尾に要素を追加する
+  // shift   : 先頭の要素を削除する
+  // pop     : 末尾の要素を削除する
+  scores.push(60, 50);
+  scores.shift();
 
-  // 文字列と一緒に配列の数値を取り出したい場合は以下のように記述します
-  // console.log(`Score: ${ scores[0] }`);
-  // console.log(`Score: ${ scores[1] }`);
-  // console.log(`Score: ${ scores[2] }`);
-
-  // 以下のようにfor文を用いることで、上記をスッキリと書くことができます
-  // for (let i = 0; i < 3; i++) { // iが０から始まって、順に１ずつ増やす
-  for (let i = 0; i < scores.length; i++) { // 配列の要素に増減があった場合、上記では３の部分を変更しなければならない
-    console.log(`Score ${i}: ${ scores[i] }`); // それに合致する数値を配列からとってきて出力する
+  for (let i = 0; i < scores.length; i++) {
+    console.log(`Score ${i}: ${ scores[i] }`);
   }
 }
