d936e15ac3d9adf46ee6151cdc19f140e16fcd76
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-27 11:54:21 +0900

    [Edit]: Know "forEach"
    Message body
    Object_8
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Object/main.js
+++ dotInstall/javascriptLesson/Object/main.js
@@ -3,17 +3,8 @@
 {
   const scores = [80, 90, 40, 70];
 
-  // const [a, b, c, d] = scores;
-  // console.log(c);
-
-  // const [a, b, ...others] = scores;
-  // console.log(others);
-
-  // 分割代入
-  let x = 30;
-  let y = 70;
-  [x, y] = [y, x]
-
-  console.log(`x: ${x}`);
-  console.log(`y: ${y}`);
+  // scores.forEach((score) => {
+  scores.forEach((score, index) => {
+    console.log(`Score ${index}: ${score}`);
+  });
 }
