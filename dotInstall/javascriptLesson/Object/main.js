3b2f2cb7ada4ffbb846a22b102f8d3872903c0db
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-29 05:55:32 +0900

    [Edit]: Manipulating strings
    Message body
    Object_17
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Object/main.js
+++ dotInstall/javascriptLesson/Object/main.js
@@ -1,25 +1,11 @@
 'use strict'
 
 {
-  // let x = 1;
-  // let y = x;
-  // x = 5;
-  // console.log(x);
-  // console.log(y);
+  const str = 'hello';
 
-  // 配列やオブジェクトの挙動
-  // 単純なデータ型と違って、データ量が大きくなる可能性があるのでデータを丸ごとコピーしてくるということはしません。
-  // すなわち、割り当てられた値そのものではなく、値がどこに格納されているかという情報だけがつくられて(x)、そこに名前（y）が付けられるという仕組み(参照しているだけ)
+  // console.log(str.length);
 
-  // let x = [ 1, 2 ];
-  // let y = x;
-  // x[0] = 5;
-  // console.log(x);
-  // console.log(y);
+  // console.log(str.substring(2, 4));
 
-  let x = [ 1, 2 ];
-  let y = [ ...x ];
-  x[0] = 5;
-  console.log(x);
-  console.log(y);
+  console.log(str[1]);
 }
