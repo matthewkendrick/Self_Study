d1a622dda04b56cb173a7f95b525a33924c37a95
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-29 08:00:47 +0900

    [Edit]: Try to create a random integer value
    Message body
    Object_20
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Object/main.js
+++ dotInstall/javascriptLesson/Object/main.js
@@ -1,23 +1,5 @@
 'use strict'
 
 {
-  const scores = [10, 3, 9];
-
-  let sum = 0;
-
-  scores.forEach(score => {
-    sum += score;
-  });
-
-  const avg = sum / scores.length;
-
-  // console.log(sum);
-  // console.log(avg);
-  
-  console.log(Math.floor(avg));
-  console.log(Math.ceil(avg));
-  console.log(Math.round(avg));
-  console.log(avg.toFixed(3));
-
-  console.log(Math.random());
+  console.log(Math.floor(Math.random() * 6) + 1); 
 }
