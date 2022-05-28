824ff69829bd78f38ed4001d0076a9674cb9fa5c
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-29 05:33:36 +0900

    [Edit]: Try assigning a variables
    Message body
    Object_15
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Object/main.js
+++ dotInstall/javascriptLesson/Object/main.js
@@ -1,22 +1,15 @@
 'use strict'
 
 {
+  // let x = 1;
+  // let y = x;
+  // x = 5;
+  // console.log(x);
+  // console.log(y);
 
-  const point = {
-    x: 100,
-    y: 180,
-  };
-
-  // const keys = Object.keys(point);
-  // keys.forEach(key => {
-  //   console.log(`Key: ${key} Value: ${point[ key ]}`);
-  // });
-
-  const points = [
-    {x: 30, y: 20},
-    {x: 10, y: 50},
-    {x: 40, y: 40},
-  ];
-
-  console.log(points[1].y);
+  // let x = [ 1, 2 ];
+  // let y = x;
+  // x[0] = 5;
+  // console.log(x);
+  // console.log(y);
 }
