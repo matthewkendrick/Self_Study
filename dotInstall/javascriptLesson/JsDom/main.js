3830621ecce0bd22d2eeb087925b2b93dcc52260
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-30 07:34:32 +0900

    [Edit]: Learn two ways to manipulate elements
    Message body
    JsDom_1-2
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/JsDom/main.js
+++ dotInstall/javascriptLesson/JsDom/main.js
@@ -1,1 +1,10 @@
 'use strict'
+
+{
+  function update() {
+  // document.querySelector('h1').textContent = 'Changed';
+  document.getElementById('target').textContent = 'Changed';
+  }
+
+  setTimeout(update, 2000)
+}
