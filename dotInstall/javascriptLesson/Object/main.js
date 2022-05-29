452ae68b8683cd9483d2beb39a66ca865270a09c
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-29 16:46:33 +0900

    [Edit]: Learn static methods that do not create instances
    Message body
    Object_33
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Object/main.js
+++ dotInstall/javascriptLesson/Object/main.js
@@ -1,13 +1,11 @@
 'use strict'
 
-
 {
   class Post {
     constructor(text) {
       this.text = text;
       this.likeCount = 0;
     }
-
     show() {
       console.log(`${this.text} - ${this.likeCount}likes`);
     }
@@ -15,18 +13,31 @@
       this.likeCount++;
       this.show();
     }
+  }
 
-    // 静的メソッド
-    static showInfo() {
-      console.log('Post class version 1.0');
+  class SponsoredPost {
+    constructor(text, sponsor) {
+      this.text = text;
+      this.likeCount = 0;
+      this.sponsor = sponsor;
+    }
+    show() {
+      console.log(`${this.text} - ${this.likeCount} likes`);
+      console.log(`...sponsored by ${this.sponsor}`);
+    }
+    like() {
+      this.likeCount++;
+      this.show();
     }
   }
 
   const posts = [
     new Post('JavaScriptの勉強中...'),
     new Post('プログラミング楽しい！'),
+    new SponsoredPost('3分動画でマスターしよう', 'dotinstall'),
   ];
 
-  posts[0].like();
-  Post.showInfo();
+  posts[2].show();
+  posts[2].like();
+  posts[2].like();
 }
