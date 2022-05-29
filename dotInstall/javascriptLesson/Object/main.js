e7d237f28fdc86795423345d8791a424cfd7e895
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-29 11:51:43 +0900

    [Edit]: Create warning and confirmation dialogs
    Message body
    Object_23
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Object/main.js
+++ dotInstall/javascriptLesson/Object/main.js
@@ -1,8 +1,12 @@
 'use strict'
 
 {
-  const d = new Date(2019, 10);
-  d.setHours(10, 20, 30);
-  d.setDate(d.getDate() + 3);
-  console.log(d);
+  // alert('Hello');
+
+  const answer = confirm('削除しますか？');
+  if (answer) {
+    console.log('削除しました');
+  } else {
+    console.log('キャンセルしました');
+  }
 }
