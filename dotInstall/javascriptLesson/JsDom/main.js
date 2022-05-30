290b2ef3cdf5164c55aa522cdf58618a1bf16c85
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-30 10:00:00 +0900

    [Edit]: Implement click events
    Message body
    JsDom_4-5
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/JsDom/main.js
+++ dotInstall/javascriptLesson/JsDom/main.js
@@ -1,12 +1,7 @@
 'use strict'
 
 {
-  function update() {
-    document.querySelectorAll('p')[0].textContent = 'Changed';
-    document.querySelectorAll('p').forEach((p, index) => {
-      p.textContent = `${index}番目のpです！`;
-    });
-  }
-
-  setTimeout(update, 2000)
+  document.querySelector('button').addEventListener('click', () => {
+    document.getElementById('target').textContent = 'Changed!';
+  });
 }
