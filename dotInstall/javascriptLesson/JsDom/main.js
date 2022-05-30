c0c67b4a863ea552fcaf7337b4b4387861e9569f
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-30 20:26:56 +0900

    [Edit]: Learn how to duplicate and insert elements
    Message body
    JsDom_11
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/JsDom/main.js
+++ dotInstall/javascriptLesson/JsDom/main.js
@@ -2,10 +2,11 @@
 
 {
   document.querySelector('button').addEventListener('click', () => {
-    const item2 = document.createElement('li');
-    item2.textContent = 'item 2';
+    const item0 = document.querySelectorAll('li')[0];
+    const copy = item0.cloneNode(true);
 
-    const ulNode = document.querySelector('ul');
-    ulNode.appendChild(item2);
+    const ul = document.querySelector('ul');
+    const item2 = document.querySelectorAll('li')[2];
+    ul.insertBefore(copy, item2)
   });
 }
