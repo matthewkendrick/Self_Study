41c0d92dc4202aaf1b87df03fde09bda2da11830
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-30 12:22:05 +0900

    [Edit]: Handling HTML custom data attributes
    Message body
    JsDom_9
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/JsDom/main.js
+++ dotInstall/javascriptLesson/JsDom/main.js
@@ -3,17 +3,7 @@
 {
   document.querySelector('button').addEventListener('click', () => {
     const targetNode = document.getElementById('target');
-    targetNode.textContent = 'Changed!';
-    targetNode.title = 'I am title!';
 
-    // targetNode.classList.add('my_color')
-    // if (targetNode.classList.contains('my_color') == true) {
-    //   targetNode.classList.remove('my_color');
-    // } else {
-    //   targetNode.classList.add('my_color')
-    // }
-
-    targetNode.classList.toggle('my_color')
-
+    targetNode.textContent = targetNode.dataset.translation;
   });
 }
