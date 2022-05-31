8010f31319485bb92e27a9b1fa9ba8ac2db077eb
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-31 10:46:55 +0900

    [Edit]: Handle events when a form is submitted
    Message body
    JsDom_20
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/JsDom/main.js
+++ dotInstall/javascriptLesson/JsDom/main.js
@@ -1,23 +1,8 @@
 'use strict'
 
 {
-  const text = document.querySelector('textarea');
-
-  // focus and blur
-  // text.addEventListener('focus', () => {
-  //   console.log('focus');
-  // });
-
-  // text.addEventListener('blur', () => {
-  //   console.log('blur');
-  // });
-
-  // input and change
-  text.addEventListener('input', () => {
-    console.log(text.value.length);
-  });
-
-  text.addEventListener('change', () => {
-    console.log('change');
+  document.querySelector('form').addEventListener('submit', e => {
+    e.preventDefault();
+    console.log('submit');
   });
 }
