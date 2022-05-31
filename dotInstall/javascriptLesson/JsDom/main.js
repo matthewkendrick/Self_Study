ec1d5b7d373e69c7967304c19cc135e1fa9763d9
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-05-31 10:56:23 +0900

    [Edit]: Understanding Event Propagation
    Message body
    JsDom_21
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/JsDom/main.js
+++ dotInstall/javascriptLesson/JsDom/main.js
@@ -1,8 +1,9 @@
 'use strict'
 
 {
-  document.querySelector('form').addEventListener('submit', e => {
-    e.preventDefault();
-    console.log('submit');
+  document.querySelector('ul').addEventListener('click', e => {
+    if (e.target.nodeName == 'LI') {
+      e.target.classList.toggle('done')
+    }
   });
 }
