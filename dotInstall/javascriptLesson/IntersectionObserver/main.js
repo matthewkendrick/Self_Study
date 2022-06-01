f8ac48044584ce2a5c6eaef636ea80dd938d2f93
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-01 14:33:33 +0900

    [Edit]: Watch when the process is executed
    Message body
    IntersectionObserver_4
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/IntersectionObserver/main.js
+++ dotInstall/javascriptLesson/IntersectionObserver/main.js
@@ -1,5 +1,13 @@
 'use strict'
 
 {
-  
+  const target = document.querySelector('img');
+
+  function callback() {
+    console.log('fired!');
+  }
+
+  const observer = new IntersectionObserver(callback);
+
+  observer.observe(target);
 }
