c84f09d99aa1008d8062a03c7b6a9d75d4c9c38d
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-02 09:25:25 +0900

    [Edit]: Class is detached when elements intersect
    Message body
    IntersectionObserver_8
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/IntersectionObserver/main.js
+++ dotInstall/javascriptLesson/IntersectionObserver/main.js
@@ -5,6 +5,18 @@
 
   function callback(entries) {
     console.log(entries[0]);
+
+    if (!entries[0].isIntersecting) {
+      return;
+    }
+
+    entries[0].target.classList.add('appear')
+
+    // if (entries[0].isIntersecting) {
+    //   entries[0].target.classList.add('appear')
+    // } else {
+    //   entries[0].target.classList.remove('appear')
+    // }
   }
 
   const options = {
