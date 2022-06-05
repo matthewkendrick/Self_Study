b771ee5975a1ecf6e36b04718c51fb71f70b1259
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-06 05:13:02 +0900

    [Edit]: Assign a class to the element to be monitored
    Message body
    PortfolioJs_3
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/PortfolioJs/main.js
+++ dotInstall/javascriptLesson/PortfolioJs/main.js
@@ -2,4 +2,15 @@
 
 {
   // Intersection Observer API
+  function callback(entries, obs) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        return;
+      }
+
+      entry.target.classList.add('appear');
+      obs.unobserve(entry.target);
+    });
+  }
+  const observer = new IntersectionObserver(callback, options);
 }
