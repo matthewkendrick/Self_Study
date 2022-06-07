3fc63b0b93b2b6b69ed641d6216caafd47fbe33e
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-08 07:52:16 +0900

    [Edit]: Implement the process of switching images after 1 second
    Message body
    EcsiteJs_4
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/EcsiteJs/main.js
+++ dotInstall/javascriptLesson/EcsiteJs/main.js
@@ -1,4 +1,12 @@
 'use strict'
 
 {
+  const images = document.querySelectorAll('.hero img');
+  let currentIndex = 0;
+
+  setTimeout(() => {
+    images[currentIndex].classList.remove('current');
+    currentIndex++;
+    images[currentIndex].classList.add('current')
+  }, 1000);
 }
