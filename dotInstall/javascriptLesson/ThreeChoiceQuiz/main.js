7ce2e3511c3e26719f9427773ace2ed07b019321
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-08 18:18:40 +0900

    [Edit]: Obtain elements and set initial data
    Message body
    ThreeChoiceQuiz_6
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/ThreeChoiceQuiz/main.js
+++ dotInstall/javascriptLesson/ThreeChoiceQuiz/main.js
@@ -1,1 +1,14 @@
 'use strict'
+
+{
+  const question = document.getElementById('question');
+  const choices = document.getElementById('choices');
+  const btn = document.getElementById('btn');
+
+  const quizSet = [
+    {q: 'What is A?', c: ['A0', 'A1', 'A2']},
+    {q: 'What is B?', c: ['B0', 'B1', 'B2']},
+    {q: 'What is C?', c: ['C0', 'C1', 'C2']},
+  ];
+  let currentNum = 0;
+}
