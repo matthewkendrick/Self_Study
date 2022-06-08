80633e436b2e64cc659d1109912885106768a6d7
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-08 11:42:00 +0900

    [Edit]: Recursively executing a function(implementation of iterative processing)
    Message body
    EcsiteJs_5
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/EcsiteJs/main.js
+++ dotInstall/javascriptLesson/EcsiteJs/main.js
@@ -1,12 +1,16 @@
 'use strict'
 
 {
+  function play() {
+    setTimeout(() => {
+      images[currentIndex].classList.remove('current');
+      currentIndex++;
+      images[currentIndex].classList.add('current')
+      play();
+    }, 1000);
+  }
   const images = document.querySelectorAll('.hero img');
   let currentIndex = 0;
 
-  setTimeout(() => {
-    images[currentIndex].classList.remove('current');
-    currentIndex++;
-    images[currentIndex].classList.add('current')
-  }, 1000);
+  play();
 }
