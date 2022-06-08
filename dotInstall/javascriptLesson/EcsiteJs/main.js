4708d15748826693a304a93940bb6158b7f43305
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-08 12:15:43 +0900

    [Edit]: Complete the slideshow
    Message body
    EcsiteJs_6
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/EcsiteJs/main.js
+++ dotInstall/javascriptLesson/EcsiteJs/main.js
@@ -5,9 +5,12 @@
     setTimeout(() => {
       images[currentIndex].classList.remove('current');
       currentIndex++;
-      images[currentIndex].classList.add('current')
+      if (currentIndex > images.length - 1 ) {
+        currentIndex = 0;
+      }
+      images[currentIndex].classList.add('current');
       play();
-    }, 1000);
+    }, 5000);
   }
   const images = document.querySelectorAll('.hero img');
   let currentIndex = 0;
