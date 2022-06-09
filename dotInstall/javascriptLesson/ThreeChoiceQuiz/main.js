10fb74ba4fc3455114b59d558d9112fdfcff6bfd
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-10 07:06:40 +0900

    [Edit]: Complete the display for score
    Message body
    ThreeChoiceQuiz_19
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/ThreeChoiceQuiz/main.js
+++ dotInstall/javascriptLesson/ThreeChoiceQuiz/main.js
@@ -4,6 +4,8 @@
   const question = document.getElementById('question');
   const choices = document.getElementById('choices');
   const btn = document.getElementById('btn');
+  const result = document.getElementById('result');
+  const scoreLabel = document.querySelector('#result > p');
 
   const quizSet = [
     {q: 'What is A?', c: ['A0', 'A1', 'A2']},
@@ -73,7 +75,8 @@
     btn.classList.add('disabled');
 
     if (currentNum == quizSet.length - 1) {
-      console.log(`Score: ${score} / ${quizSet.length}`);
+      scoreLabel.textContent = `Score: ${score} / ${quizSet.length}`;
+      result.classList.remove('hidden');
     } else {
       currentNum++;
       setQuiz();
