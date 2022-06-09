c860fa0bd18d3c16a6234554a5f8e07a54147675
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-09 10:43:27 +0900

    [Edit]: Allow me to move on to the next quiz
    Message body
    ThreeChoiceQuiz_13
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/ThreeChoiceQuiz/main.js
+++ dotInstall/javascriptLesson/ThreeChoiceQuiz/main.js
@@ -24,7 +24,6 @@
   }
 
   function checkAnswer(li) {
-    // if (isAnswered == true) {
     if (isAnswered) {
       return;
     }
@@ -35,11 +34,18 @@
     } else {
       li.classList.add('wrong');
     }
+
+    btn.classList.remove('disabled');
   }
 
   function setQuiz() {
     isAnswered = false;
+
     question.textContent = quizSet[currentNum].q;
+
+    while (choices.firstChild) {
+      choices.removeChild(choices.firstChild);
+    }
 
     const shuffledChoices = shuffle([...quizSet[currentNum].c]);
     shuffledChoices.forEach(choice => {
@@ -53,4 +59,9 @@
   }
 
   setQuiz();
+
+  btn.addEventListener('click', () => {
+    currentNum++;
+    setQuiz();
+  });
 }
