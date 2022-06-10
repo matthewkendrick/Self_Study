2f94c5b639be4cbfe3439bf37021455a5fac0806
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-10 16:04:20 +0900

    [Edit]: Function to summarize the process
    Message body
    BingoSheet_6
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/BingoSheet/main.js
+++ dotInstall/javascriptLesson/BingoSheet/main.js
@@ -1,22 +1,32 @@
 'use strict';
 
 {
-  const source = [];
-  for (let i = 0; i < 15; i++) {
-    source[i] = i + 1;
-  }
+  function createColumn(col) {
+    const source = [];
+    for (let i = 0; i < 15; i++) {
+      source[i] = i + 1 + 15 * col;
+    }
 
-  const b = [];
-  // b[0] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[1] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[2] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[3] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  // b[4] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+    const b = [];
+    // b[0] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+    // b[1] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+    // b[2] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+    // b[3] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+    // b[4] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
 
-  for (let i = 0; i < 5; i++) {
-    b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-    
-  }
+    for (let i = 0; i < 5; i++) {
+      b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+      
+    }
 
-  console.log(b);
+    console.log(b);
+      
+    }
+
+  const columns = [];
+  columns[0] = createColumn(0);
+  columns[1] = createColumn(1);
+  columns[2] = createColumn(2);
+  columns[3] = createColumn(3);
+  columns[4] = createColumn(0);
 }
