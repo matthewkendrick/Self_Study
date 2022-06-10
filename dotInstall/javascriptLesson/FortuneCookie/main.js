6f2c277ccdf815b4cfe0a7650ea60b5620cf0a4f
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-10 11:52:22 +0900

    [Edit]: Improved implementation using arrays
    Message body
    FortuneCookie_8
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/FortuneCookie/main.js
+++ dotInstall/javascriptLesson/FortuneCookie/main.js
@@ -4,18 +4,8 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    const n = Math.floor(Math.random() * 3);
+    const results = ['大吉', '中吉', '凶', '末吉'];
 
-    switch (n) {
-      case 0:
-        btn.textContent = '大吉';
-        break;
-      case 1:
-        btn.textContent = '中吉';
-        break;
-      case 2:
-        btn.textContent = '凶';
-        break;
-    }
+    btn.textContent = results[Math.floor(Math.random() * results.length)];
   });
 }
