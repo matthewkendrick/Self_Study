6501053fb7675da168e317e289e62f98d32bcaa3
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-10 12:03:57 +0900

    [Edit]: Improved to allow manipulation of the probability of a fortune
    Message body
    FortuneCookie_9
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/FortuneCookie/main.js
+++ dotInstall/javascriptLesson/FortuneCookie/main.js
@@ -4,8 +4,15 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    const results = ['大吉', '中吉', '凶', '末吉'];
-
-    btn.textContent = results[Math.floor(Math.random() * results.length)];
+    const n = Math.random();
+    if (n < 0.05) {
+      btn.textContent = '大吉';
+    } else if (n < 0.2) {
+      btn.textContent = '中吉';
+    } else if (n < 0.4) {
+      btn.textContent = '末吉'
+    } else {
+      btn.textContent = '凶';
+    }
   });
 }
