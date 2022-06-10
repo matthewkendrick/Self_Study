81da7b43426ca149001bc4c30f0bc40e2255164c
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-10 15:45:51 +0900

    [Edit]: Create an array of column B
    Message body
    BingoSheet_2-3
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/BingoSheet/main.js
+++ dotInstall/javascriptLesson/BingoSheet/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+{
+  const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+  const b = [];
+  b[0] = source.splice(Math.floor(Math.random() * source.length), 1);
+  b[1] = source.splice(Math.floor(Math.random() * source.length), 1);
+  b[2] = source.splice(Math.floor(Math.random() * source.length), 1);
+  b[3] = source.splice(Math.floor(Math.random() * source.length), 1);
+  b[4] = source.splice(Math.floor(Math.random() * source.length), 1);
+
+  console.log(b);
+}
