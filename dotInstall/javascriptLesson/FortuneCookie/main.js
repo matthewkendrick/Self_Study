833c2794a94580fa494d842cea8b4782d4a13538
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-10 11:44:19 +0900

    [Edit]: Implemented fortune-telling by switch statement
    Message body
    FortuneCookie_6-7
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/FortuneCookie/main.js
+++ dotInstall/javascriptLesson/FortuneCookie/main.js
@@ -4,6 +4,18 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    btn.textContent = 'hit';
+    const n = Math.floor(Math.random() * 3);
+
+    switch (n) {
+      case 0:
+        btn.textContent = '大吉';
+        break;
+      case 1:
+        btn.textContent = '中吉';
+        break;
+      case 2:
+        btn.textContent = '凶';
+        break;
+    }
   });
 }
