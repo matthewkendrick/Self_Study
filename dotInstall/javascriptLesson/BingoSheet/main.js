1a56239e66e56686d111937d40e2c9b75033bd52
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-10 15:56:33 +0900

    [Edit]: Use a for statement to simplify the description
    Message body
    BingoSheet_5
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/BingoSheet/main.js
+++ dotInstall/javascriptLesson/BingoSheet/main.js
@@ -1,14 +1,22 @@
 'use strict';
 
 {
-  const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  const source = [];
+  for (let i = 0; i < 15; i++) {
+    source[i] = i + 1;
+  }
 
   const b = [];
-  b[0] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  b[1] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  b[2] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  b[3] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  b[4] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+  // b[0] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+  // b[1] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+  // b[2] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+  // b[3] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+  // b[4] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+
+  for (let i = 0; i < 5; i++) {
+    b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+    
+  }
 
   console.log(b);
 }
