5dc22db49a18e9eb6c9751a110d7bc0c929af7f0
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-10 15:49:52 +0900

    [Edit]: Verified with developer tools
    Message body
    BingoSheet_4
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/BingoSheet/main.js
+++ dotInstall/javascriptLesson/BingoSheet/main.js
@@ -4,11 +4,11 @@
   const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
   const b = [];
-  b[0] = source.splice(Math.floor(Math.random() * source.length), 1);
-  b[1] = source.splice(Math.floor(Math.random() * source.length), 1);
-  b[2] = source.splice(Math.floor(Math.random() * source.length), 1);
-  b[3] = source.splice(Math.floor(Math.random() * source.length), 1);
-  b[4] = source.splice(Math.floor(Math.random() * source.length), 1);
+  b[0] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+  b[1] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+  b[2] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+  b[3] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+  b[4] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
 
   console.log(b);
 }
