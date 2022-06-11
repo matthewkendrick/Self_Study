4328428ca39d3e83f5f3ac70cc91eb3d126dbc84
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-12 05:12:30 +0900

    [Edit]: Get the current month's date and insert it into the array
    Message body
    Calendar_6
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Calendar/main.js
+++ dotInstall/javascriptLesson/Calendar/main.js
@@ -3,4 +3,17 @@
 {
   const year = 2020;
   const month = 4; //0から数えるので5月　=　4
+  
+  function getCalendarBody() {
+    const dates = [];
+    const lastDate = new Date(year, month +1, 0).getDate();
+
+    for (let i = 0; i <= lastDate; i++) {
+      dates.push(i);
+    }
+
+    console.log(dates);
+  }
+
+  getCalendarBody();
 }
