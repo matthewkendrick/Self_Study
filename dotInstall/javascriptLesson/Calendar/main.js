f9f12db1c17449e5997e47e909cb90c03761b34a
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-12 05:16:31 +0900

    [Edit]: Rewriting array contents to object
    Message body
    Calendar_7
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Calendar/main.js
+++ dotInstall/javascriptLesson/Calendar/main.js
@@ -1,15 +1,21 @@
 'use strict';
 
+console.clear();
+
 {
   const year = 2020;
   const month = 4; //0から数えるので5月　=　4
   
   function getCalendarBody() {
     const dates = [];
-    const lastDate = new Date(year, month +1, 0).getDate();
+    const lastDate = new Date(year, month + 1, 0).getDate();
 
     for (let i = 0; i <= lastDate; i++) {
-      dates.push(i);
+      dates.push({
+        date: i,
+        isToday: false,
+        isDisable: false,
+      });
     }
 
     console.log(dates);
