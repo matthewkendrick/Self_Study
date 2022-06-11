2420fd65ba83a0886a6f071c0266aecbd11529e2
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-12 05:24:41 +0900

    [Edit]: Put last month's date in the array
    Message body
    Calendar_8
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Calendar/main.js
+++ dotInstall/javascriptLesson/Calendar/main.js
@@ -5,6 +5,22 @@ console.clear();
 {
   const year = 2020;
   const month = 4; //0から数えるので5月　=　4
+
+  function getCalendarHead() {
+    const dates = [];
+    const d = new Date(year, month, 0).getDate();
+    const n = new Date(year, month, 1).getDay();
+
+    for (let i = 0; i < n; i++) {
+      dates.unshift({
+        date: d - i,
+        isToday: false,
+        isDisabled: true
+      });
+    }
+
+    console.log(dates);
+  }
   
   function getCalendarBody() {
     const dates = [];
@@ -14,12 +30,12 @@ console.clear();
       dates.push({
         date: i,
         isToday: false,
-        isDisable: false,
+        isDisabled: false,
       });
     }
 
     console.log(dates);
   }
 
-  getCalendarBody();
+  getCalendarHead();
 }
