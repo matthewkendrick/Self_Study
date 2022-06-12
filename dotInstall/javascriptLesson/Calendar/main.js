bba40a828789d9124078afb316879c828f6d7475
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-13 08:11:23 +0900

    [Edit]: Set up click events for PREV and NEXT
    Message body
    Calendar_14
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Calendar/main.js
+++ dotInstall/javascriptLesson/Calendar/main.js
@@ -3,8 +3,8 @@
 console.clear();
 
 {
-  const year = 2020;
-  const month = 4; //0から数えるので5月　=　4
+  let year = 2020;
+  let month = 4; //0から数えるので5月　=　4
 
   function getCalendarHead() {
     const dates = [];
@@ -81,6 +81,24 @@ console.clear();
       document.querySelector('tbody').appendChild(tr);
     });
   }
+
+  document.getElementById('prev').addEventListener('click', () => {
+    month++;
+    if (month < 0) {
+      year--;
+      month = 11;
+    }
+  });
+
+  document.getElementById('next').addEventListener('click', () => {
+    month++;
+    if (month > 11) {
+      year++;
+      month = 0;
+    }
+
+    createCalendar();
+  });
 
   createCalendar();
 
