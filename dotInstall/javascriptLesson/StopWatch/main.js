42977c9fa30e3e9449cb82897f73367b991c16f6
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-13 11:24:23 +0900

    [Edit]: Fixed number of digits to display
    Message body
    StopWatch_4
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/StopWatch/main.js
+++ dotInstall/javascriptLesson/StopWatch/main.js
@@ -9,7 +9,11 @@
   let startTime;
 
   function countUp() {
-    console.log(Date.now() - startTime);
+    const d = new Date(Date.now() - startTime);
+    const m = String( d.getMinutes() ).padStart(2, '0');
+    const s = String( d.getSeconds() ).padStart(2, '0');
+    const ms = String( d.getMilliseconds() ).padStart(3, '0');
+    timer.textContent = `${m}:${s}.${ms}`;
 
     setTimeout(() => {
       countUp();
