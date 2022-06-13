4202ebad4abf02fe8a4d3e12836444fdd9df13b6
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-13 11:17:44 +0900

    [Edit]: Implement a timer count-up function
    Message body
    StopWatch_3
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/StopWatch/main.js
+++ dotInstall/javascriptLesson/StopWatch/main.js
@@ -5,4 +5,19 @@
   const start = document.getElementById('start');
   const stop = document.getElementById('stop');
   const reset = document.getElementById('reset');
+
+  let startTime;
+
+  function countUp() {
+    console.log(Date.now() - startTime);
+
+    setTimeout(() => {
+      countUp();
+    }, 10);
+  }
+
+  start.addEventListener('click', () => {
+    startTime = Date.now();
+    countUp();
+  });
 }
