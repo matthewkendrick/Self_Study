ef08f7e35da9501e025a0441f40d87272a1128ce
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-13 16:04:17 +0900

    [Edit]: Prepared conditional branches for correct and incorrect decisions
    Message body
    TypingGame_4
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/TypingGame/main.js
+++ dotInstall/javascriptLesson/TypingGame/main.js
@@ -1,8 +1,15 @@
 'use strict';
 
 {
+  const word = 'red';
+  let loc = 0;
+
+  const target = document.getElementById('target');
+  target.textContent = word;
+
   document.addEventListener('keydown', e => {
-    const target = document.getElementById('target');
-    target.textContent = e.key;
+    if (e.key == word[loc]) {
+      loc++;
+    }
   });
 }
