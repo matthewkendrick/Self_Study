def0f223373442de39df90e381c7b42235dbd14c
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-13 11:12:23 +0900

    [Edit]: Prepared timer markup and retrieval of elements
    Message body
    StopWatch_2
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/StopWatch/main.js
+++ dotInstall/javascriptLesson/StopWatch/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+{
+  const timer = document.getElementById('timer');
+  const start = document.getElementById('start');
+  const stop = document.getElementById('stop');
+  const reset = document.getElementById('reset');
+}
