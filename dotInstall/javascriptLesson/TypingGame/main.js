c337758b27c6a224d2998b4eee1ad5ce333a1868
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-13 19:01:20 +0900

    [Edit]: Corresponding to multiple words
    Message body
    TypingGame_7
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/TypingGame/main.js
+++ dotInstall/javascriptLesson/TypingGame/main.js
@@ -1,10 +1,16 @@
 'use strict';
 
 {
-  const word = 'red';
+  const words = [
+    'red',
+    'blue',
+    'pink',
+  ];
+  let word = 'red';
   let loc = 0;
 
   const target = document.getElementById('target');
+  word = words[Math.floor(Math.random() * words.length)];
   target.textContent = word;
 
   document.addEventListener('keydown', e => {
