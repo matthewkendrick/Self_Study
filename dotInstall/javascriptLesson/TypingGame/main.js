0a007240d548d8f1dd6c651032282cc08f1eb4eb
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-13 19:08:21 +0900

    [Edit]: The next word is set to be set
    Message body
    TypingGame_8
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/TypingGame/main.js
+++ dotInstall/javascriptLesson/TypingGame/main.js
@@ -1,6 +1,12 @@
 'use strict';
 
 {
+  function setWord() {
+    word = words[Math.floor(Math.random() * words.length)];
+    target.textContent = word;
+    loc = 0;
+  }
+
   const words = [
     'red',
     'blue',
@@ -10,8 +16,8 @@
   let loc = 0;
 
   const target = document.getElementById('target');
-  word = words[Math.floor(Math.random() * words.length)];
-  target.textContent = word;
+
+  setWord();
 
   document.addEventListener('keydown', e => {
     if (e.key != word[loc]) {
@@ -21,5 +27,9 @@
     loc++;
 
     target.textContent = '_'.repeat(loc) + word.substring(loc);
+
+    if (loc == word.length) {
+      setWord();
+    }
   });
 }
