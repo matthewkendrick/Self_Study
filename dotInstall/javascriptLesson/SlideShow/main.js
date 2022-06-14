e052a63a6c1a80a7cb348934005293641c50260b
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-15 05:48:00 +0900

    [Edit]: Use JavaScript to embed the main image
    Message body
    SlideShow_7
    
    Message footera
    Self-Study

--- dotInstall/javascriptLesson/SlideShow/main.js
+++ dotInstall/javascriptLesson/SlideShow/main.js
@@ -1,4 +1,18 @@
 'use strict';
 
 {
+  const images = [
+    'img/pic00.png',
+    'img/pic01.png',
+    'img/pic02.png',
+    'img/pic03.png',
+    'img/pic04.png',
+    'img/pic05.png',
+    'img/pic06.png',
+    'img/pic07.png',
+  ];
+  let currentIndex = 0;
+
+  const mainImage = document.getElementById('main');
+  mainImage.src = images[currentIndex];
 }
