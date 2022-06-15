6e838abdb4df90c9764b5367ca8771a59816ab3c
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-15 18:31:59 +0900

    [Edit]: Rewritten to generate elements in the constructor
    Message body
    SlotMachine_6
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/SlotMachine/main.js
+++ dotInstall/javascriptLesson/SlotMachine/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-{}
+{
+  class Panel {
+    constructor() {
+      const section = document.createElement('section');
+      section.classList.add('panel');
+
+      this.img = document.createElement('img');
+      this.img.src = 'img/seven.png';
+
+      this.stop = document.createElement('div');
+      this.stop.textContent = 'STOP';
+      this.stop.classList.add('stop');
+    }
+  }
+}
