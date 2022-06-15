2886a231a88d01edffd60810019586115e238577
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-15 18:45:03 +0900

    [Edit]: Pressing the spin button switches the image at random
    Message body
    SlotMachine_7
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/SlotMachine/main.js
+++ dotInstall/javascriptLesson/SlotMachine/main.js
@@ -19,6 +19,19 @@
       const main = document.querySelector('main');
       main.appendChild(section);
     }
+
+    getRandomImage() {
+      const images = [
+        'img/seven.png',
+        'img/bell.png',
+        'img/cherry.png',
+      ];
+      return images[Math.floor(Math.random() * images.length)];
+    }
+
+    spin() {
+      this.img.src = this.getRandomImage();
+    }
   }
 
   const panels = [
@@ -26,4 +39,11 @@
     new Panel(),
     new Panel(),
   ];
+
+  const spin = document.getElementById('spin');
+  spin.addEventListener('click', () => {
+    panels.forEach(panel => {
+      panel.spin();
+    })
+  });
 }
