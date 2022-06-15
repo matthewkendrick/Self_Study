ff464686493e266b40f26520b8b077dfb55be426
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-16 05:19:28 +0900

    [Edit]: Reset process to allow repeated playback
    Message body
    SlotMachine_12
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/SlotMachine/main.js
+++ dotInstall/javascriptLesson/SlotMachine/main.js
@@ -13,7 +13,7 @@
 
       this.stop = document.createElement('div');
       this.stop.textContent = 'STOP';
-      this.stop.classList.add('stop');
+      this.stop.classList.add('stop', 'inactive');
       this.stop.addEventListener('click', () => {
         if (this.stop.classList.contains('inactive')) {
           return;
@@ -24,6 +24,8 @@
         panelsLeft--;
 
         if (panelsLeft == 0) {
+          spin.classList.remove('inactive');
+          panelsLeft = 3;
           checkResult();
         }
       });
@@ -58,6 +60,11 @@
       unmatch() {
         this.img.classList.add('unmatched');
       }
+
+      activate() {
+        this.img.classList.remove('unmatched');
+        this.stop.classList.remove('inactive');
+      }
     }
 
   function checkResult() {
@@ -87,6 +94,7 @@
     }
     spin.classList.add('inactive');
     panels.forEach(panel => {
+      panel.activate();
       panel.spin();
     })
   });
