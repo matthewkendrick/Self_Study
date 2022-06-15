7e8f42e7fa6bd4d820d5a84baf25f257c7c73b38
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-16 07:29:40 +0900

    [Edit]: Prepared to generate elements using JS
    Message body
    NumbersGame_7
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/NumbersGame/main.js
+++ dotInstall/javascriptLesson/NumbersGame/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-{}
+{
+  class Panel {
+    constructor() {
+      this.el = document.createElement('li');
+      this.el.classList.add('pressed');
+    }
+  }
+
+  class Board {
+    constructor() {
+      this.panels = [];
+      for (let i = 0; i < 4; i++) {
+        this.panels.push(new Panels());
+      }
+    }
+  }
+
+  const board = new Board();
+}
