1eff082ea4abccc40753636d6dc0eb7f431de8b7
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-16 07:34:27 +0900

    [Edit]: Add the generated panel to the page
    Message body
    NumbersGame_8
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/NumbersGame/main.js
+++ dotInstall/javascriptLesson/NumbersGame/main.js
@@ -6,14 +6,25 @@
       this.el = document.createElement('li');
       this.el.classList.add('pressed');
     }
+
+    getEl() {
+      return this.el;
+    }
   }
 
   class Board {
     constructor() {
       this.panels = [];
       for (let i = 0; i < 4; i++) {
-        this.panels.push(new Panels());
+        this.panels.push(new Panel());
       }
+      this.setup();
+    }
+    setup() {
+      const board = document.getElementById('board');
+      this.panels.forEach(panel => {
+        board.appendChild(panel.getEl());
+      });
     }
   }
 
