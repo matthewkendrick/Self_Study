bc050c77aaa27bd40fd7ee4c710bb45b020f9a19
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-17 06:02:48 +0900

    [Edit]: Get the elements with JavaScript
    Message body
    ModalWindow_6
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/ModalWindow/main.js
+++ dotInstall/javascriptLesson/ModalWindow/main.js
@@ -1,3 +1,8 @@
 'use strict';
 
-{}
+{
+  const open = document.getElementById('open');
+  const close = document.getElementById('close');
+  const modal = document.getElementById('modal');
+  const mask = document.getElementById('mask');
+}
