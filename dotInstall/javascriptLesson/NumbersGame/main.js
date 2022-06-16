104dd50e0c4a6a13a1284cf63428aff6372ce512
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-16 10:05:43 +0900

    [Edit]: START button to start the game
    Message body
    NumbersGame_9
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/NumbersGame/main.js
+++ dotInstall/javascriptLesson/NumbersGame/main.js
@@ -10,6 +10,11 @@
     getEl() {
       return this.el;
     }
+
+    activate(num) {
+      this.el.classList.remove('pressed');
+      this.el.textContent = num;
+    }
   }
 
   class Board {
@@ -26,7 +31,18 @@
         board.appendChild(panel.getEl());
       });
     }
+
+    activate() {
+      this.panels.forEach(panel => {
+        panel.activate(0);
+      });
+    }
   }
 
   const board = new Board();
+
+  const btn = document.getElementById('btn');
+  btn.addEventListener('click', () => {
+    board.activate();
+  });
 }
