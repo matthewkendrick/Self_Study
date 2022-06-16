3c38ccd8b6b1a4f96efd920cbf12d0cacdc3d9f0
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-17 07:56:19 +0900

    [Edit]: Added click event and hidden class can be attached or detached
    Message body
    ModalWindow_7
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/ModalWindow/main.js
+++ dotInstall/javascriptLesson/ModalWindow/main.js
@@ -5,4 +5,18 @@
   const close = document.getElementById('close');
   const modal = document.getElementById('modal');
   const mask = document.getElementById('mask');
+
+  open.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    mask.classList.remove('hidden');
+  });
+
+  close.addEventListener('click', () => {
+    modal.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+
+  mask.addEventListener('click', () => {
+    close.click();
+  });
 }
