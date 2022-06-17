66347ed5532351d799e9a3439731859e435c6914
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-18 05:41:50 +0900

    [Edit]: Set up a circular gradient
    Message body
    Canvas_7
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Canvas/main.js
+++ dotInstall/javascriptLesson/Canvas/main.js
@@ -7,11 +7,15 @@
       return;
     }
     const ctx = canvas.getContext('2d');
-    const g = ctx.createLinearGradient(0, 0, canvas.width, 0);
+
+    const g = ctx.createRadialGradient(
+      canvas.width / 2, canvas.height / 2, 50,
+      canvas.width / 2 + 100, canvas.height / 2 + 200, 500
+    );
 
     g.addColorStop(0, '#f00');
-    g.addColorStop(0.3, '#0f0');
-    g.addColorStop(1, '00f');
+    g.addColorStop(0.1, '#0f0');
+    g.addColorStop(1, '#00f');
 
     ctx.fillStyle = g;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
