97bbe6b87ec4ee959a10545b60032d03165c56e6
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-18 05:31:33 +0900

    [Edit]: Set up a linear gradient
    Message body
    Canvas_6
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Canvas/main.js
+++ dotInstall/javascriptLesson/Canvas/main.js
@@ -7,16 +7,14 @@
       return;
     }
     const ctx = canvas.getContext('2d');
+    const g = ctx.createLinearGradient(0, 0, canvas.width, 0);
 
-    ctx.fillStyle = 'pink';
-    ctx.strokeStyle = '#f00';
-    ctx.fillRect(50, 50, 50, 50); //塗りつぶしの四角
-    ctx.strokeRect(50, 50, 50, 50); //枠線の四角
+    g.addColorStop(0, '#f00');
+    g.addColorStop(0.3, '#0f0');
+    g.addColorStop(1, '00f');
 
-    ctx.fillStyle = 'skyblue';
-    ctx.strokeStyle = '#00b';
-    ctx.fillRect(70, 70, 50, 50); //塗りつぶしの四角
-    ctx.strokeRect(70, 70, 50, 50); //枠線の四角
+    ctx.fillStyle = g;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
   }
   draw();
 }
