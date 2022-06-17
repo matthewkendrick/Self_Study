bf880fcaa282dd701289b756b538f95d64c56cc7
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-18 05:18:34 +0900

    [Edit]: Learn about Re-Styling
    Message body
    Canvas_5
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Canvas/main.js
+++ dotInstall/javascriptLesson/Canvas/main.js
@@ -9,11 +9,14 @@
     const ctx = canvas.getContext('2d');
 
     ctx.fillStyle = 'pink';
-    ctx.fillRect(50, 50, 50, 50); //塗りつぶしの四角
     ctx.strokeStyle = '#f00';
-    ctx.lineWidth = 8;
-    ctx.lineJoin = 'round';
-    ctx.strokeRect(105, 50, 50, 50); //枠線の四角
+    ctx.fillRect(50, 50, 50, 50); //塗りつぶしの四角
+    ctx.strokeRect(50, 50, 50, 50); //枠線の四角
+
+    ctx.fillStyle = 'skyblue';
+    ctx.strokeStyle = '#00b';
+    ctx.fillRect(70, 70, 50, 50); //塗りつぶしの四角
+    ctx.strokeRect(70, 70, 50, 50); //枠線の四角
   }
   draw();
 }
