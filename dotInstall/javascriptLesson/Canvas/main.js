5a9d25301f7a5ab2f2219692f7c4f947fab62f38
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-18 05:47:32 +0900

    [Edit]: Know how to shadow a figure
    Message body
    Canvas_8
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Canvas/main.js
+++ dotInstall/javascriptLesson/Canvas/main.js
@@ -8,17 +8,12 @@
     }
     const ctx = canvas.getContext('2d');
 
-    const g = ctx.createRadialGradient(
-      canvas.width / 2, canvas.height / 2, 50,
-      canvas.width / 2 + 100, canvas.height / 2 + 200, 500
-    );
+    ctx.shadowOffsetX = 4;
+    ctx.shadowOffsetY = 4;
+    ctx.shadowBlur = 4;
+    ctx.shadowColor = 'rgba(0, 0, 0, 0.3)';
 
-    g.addColorStop(0, '#f00');
-    g.addColorStop(0.1, '#0f0');
-    g.addColorStop(1, '#00f');
-
-    ctx.fillStyle = g;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(50, 50, 50, 50);
   }
   draw();
 }
