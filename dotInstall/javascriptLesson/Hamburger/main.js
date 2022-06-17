5468ee612cbee69accaf7d7147acada38d5437b0
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-17 18:32:07 +0900

    [Edit]: Preparing the JS
    Message body
    Hamburger_8
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Hamburger/main.js
+++ dotInstall/javascriptLesson/Hamburger/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-{}
+{
+  const open = document.getElementById('open');
+  const overlay = document.querySelector('.overlay');
+
+  open.addEventListener('click', () => {
+    
+  });
+}
