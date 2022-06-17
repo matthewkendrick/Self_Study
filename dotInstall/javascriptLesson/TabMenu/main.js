2db4f7a9b7dc6d3abdcdae303a07122a8feaf6db
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-17 10:32:11 +0900

    [Edit]: Set the active class to be added when the tab is clicked
    Message body
    TabMenu_7
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/TabMenu/main.js
+++ dotInstall/javascriptLesson/TabMenu/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-{}
+{
+  const menuItems = document.querySelectorAll('.menu li a');
+
+  menuItems.forEach(item => {
+    item.addEventListener('click', e => {
+      e.preventDefault();
+      
+      item.classList.add('active');
+    });
+  });
+}
