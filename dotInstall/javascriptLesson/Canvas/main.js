3a481a2f25d16a3565b497231cfab901be9cb05c
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-18 06:04:20 +0900

    [Edit]: Know how to draw various arcs using the arc method
    Message body
    Canvas_11
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Canvas/main.js
+++ dotInstall/javascriptLesson/Canvas/main.js
@@ -9,22 +9,8 @@
     const ctx = canvas.getContext('2d');
 
     ctx.beginPath();
-    ctx.moveTo(100, 50);
-    ctx.lineTo(200, 50);
-    ctx.setLineDash([5, 10]);
-    ctx.stroke();
-
-    ctx.beginPath();
     ctx.moveTo(100, 100);
-    ctx.lineTo(200, 100);
-    ctx.setLineDash([]);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(100, 150);
-    ctx.lineTo(200, 150);
-    ctx.lineWidth = 16;
-    ctx.lineCap = 'round';
+    ctx.arc(100, 100, 50, 0, 300 / 180 * Math.PI);
     ctx.stroke();
   }
   draw();
