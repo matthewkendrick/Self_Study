8dea6d59959eb6ff1e845b9396c27b187bfd5c2b
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-17 22:23:08 +0900

    [Edit]: Obtain a context object and draw a rectangle
    Message body
    Canvas_3
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Canvas/main.js
+++ dotInstall/javascriptLesson/Canvas/main.js
@@ -2,6 +2,14 @@
 
 {
   function draw() {
+    const canvas = document.querySelector('canvas');
+    if (typeof canvas.getContext === 'undefined') {
+      return;
+    }
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillRect(50, 50, 50, 50); //塗りつぶしの四角
+    ctx.strokeRect(105, 50, 50, 50); //枠線の四角
   }
   draw();
 }
