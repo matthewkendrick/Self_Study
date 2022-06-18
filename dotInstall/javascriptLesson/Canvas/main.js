09b9cadaa2f7fc1612f6eb0333d5f012a8468a73
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-19 06:06:10 +0900

    [Edit]: Know how to support high-resolution displays
    Message body
    Canvas_20
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Canvas/main.js
+++ dotInstall/javascriptLesson/Canvas/main.js
@@ -10,6 +10,18 @@
     }
     const ctx = canvas.getContext('2d');
 
+    const CANVAS_WIDTH = 600;
+    const CANVAS_HEIGHT = 240;
+    const dpr = window.devicePixelRatio || 1;
+    canvas.width = CANVAS_WIDTH * dpr;
+    canvas.height = CANVAS_HEIGHT * dpr;
+    ctx.scale(dpr, dpr);
+
+    ctx.font = 'bold 48px Verdana';
+    ctx.strokeText('Tokyo', 200, 200);
+    canvas.style.width = CANVAS_WIDTH + 'px';
+    canvas.style.height = CANVAS_HEIGHT + 'px';
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
     ctx.ellipse(100, 100, 40, 30, 0, 0, 2 * Math.PI);
