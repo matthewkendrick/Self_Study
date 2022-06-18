bbd1b228eff65a1bc1656da005f1ba88c68de0cb
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-19 06:22:22 +0900

    [Edit]: Obtained the canvas element and prepared to create the Icon class
    Message body
    LoadingIcon_2
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/LoadingIcon/main.js
+++ dotInstall/javascriptLesson/LoadingIcon/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-{}
+(() => {
+  class icon {
+    constructor(canvas) {
+      
+    }
+    run() {
+
+    }
+  }
+  const canvas = document.querySelector('canvas');
+  if (typeof canvas.getContext === 'undefined') {
+    return;
+  }
+
+  const icon = new Icon(canvas);
+  icon.run();
+})();
