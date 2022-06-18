ad1bcaca801c6db373007c2d7644675060da6670
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-19 05:56:43 +0900

    [Edit]: Animation a character's eyes using trigonometric functions
    Message body
    Canvas_19
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/Canvas/main.js
+++ dotInstall/javascriptLesson/Canvas/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
 {
+  let t = 0;
+
   function draw() {
     const canvas = document.querySelector('canvas');
     if (typeof canvas.getContext === 'undefined') {
@@ -8,44 +10,20 @@
     }
     const ctx = canvas.getContext('2d');
 
-    ctx.fillStyle = 'pink';
-    ctx.fillRect(0, 0, 200, 200);
-
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
     ctx.ellipse(100, 100, 40, 30, 0, 0, 2 * Math.PI);
     ctx.fillStyle = 'black';
-    ctx.fill();
+    ctx.fill()
 
     ctx.beginPath();
-    ctx.ellipse(80, 100, 8, 8, 0, 0, 2 * Math.PI);
-    ctx.ellipse(120, 100, 8, 8, 0, 0, 2 * Math.PI);
+    ctx.ellipse(80 + Math.sin(t / 30), 100, 8, 8, 0, 0, 2 * Math.PI);
+    ctx.ellipse(120 + Math.sin(t / 30), 100, 8, 8, 0, 0, 2 * Math.PI);
     ctx.fillStyle = 'skyblue';
-    ctx.fill();
+    ctx.fill()
 
-    ctx.save();
-
-    ctx.scale(0.5, 0.5);
-    ctx.translate(400, 0);
-    ctx.rotate(45 / 180 * Math.PI);
-
-    ctx.fillStyle = 'olive';
-    ctx.fillRect(0, 0, 200, 200);
-
-    ctx.beginPath();
-    ctx.ellipse(100, 100, 40, 30, 0, 0, 2 * Math.PI);
-    ctx.fillStyle = 'black';
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.ellipse(80, 100, 8, 8, 0, 0, 2 * Math.PI);
-    ctx.ellipse(120, 100, 8, 8, 0, 0, 2 * Math.PI);
-    ctx.fillStyle = 'skyblue';
-    ctx.fill();
-
-    ctx.restore();
-
-    ctx.fillStyle = 'black';
-    ctx.fillRect(80, 120, 40, 40);
+    t++;
+    setTimeout(draw, 10);
   }
   draw();
 }
