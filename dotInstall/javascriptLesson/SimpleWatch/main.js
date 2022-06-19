cea19aa3103a45ee8106d234b3db6783f42ee09b
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-19 22:13:58 +0900

    [Edit]: Create a Clock class and prepare for drawing
    Message body
    SimpleWatch_2
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/SimpleWatch/main.js
+++ dotInstall/javascriptLesson/SimpleWatch/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-{}
+{
+  class Clock {
+    drawFace() {
+      const canvas = document.querySelector('canvas');
+      if (typeof canvas.getContext == 'undefined') {
+        return;
+      }
+      const ctx = canvas.getContext('2d');
+
+      const width = canvas.width;
+      const height = canvas.height;
+    }
+
+    run() {
+      this.drawFace();
+    }
+  }
+
+  const clock = new Clock();
+  clock.run();
+}
