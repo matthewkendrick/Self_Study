7e0dff5a595f84f93619d11088d8cc9b6e7cec1f
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-19 12:33:14 +0900

    [Edit]: The circle was drawn
    Message body
    LoadingIcon_3
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/LoadingIcon/main.js
+++ dotInstall/javascriptLesson/LoadingIcon/main.js
@@ -1,14 +1,24 @@
 'use strict';
 
 (() => {
-  class icon {
+  class Icon {
     constructor(canvas) {
-      
+      this.ctx = canvas.getContext('2d');
+      this.width = canvas.width;
+      this.height = canvas.height;
+      this.r = 30;
     }
-    run() {
+    draw() {
+      this.ctx.beginPath();
+      this.ctx.arc(this.width / 2, this.height / 2, this.r, 0, 2 * Math.PI);
+      this.ctx.stroke();
+    }
 
+    run() {
+      this.draw();
     }
   }
+
   const canvas = document.querySelector('canvas');
   if (typeof canvas.getContext === 'undefined') {
     return;
