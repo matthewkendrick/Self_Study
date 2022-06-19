f1fc75b8866bba44bcc9e076880a15da04a5fd65
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-20 08:25:19 +0900

    [Edit]: Organized the ClockDrawer class and introduced immediate functions
    Message body
    SimpleWatch_6
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/SimpleWatch/main.js
+++ dotInstall/javascriptLesson/SimpleWatch/main.js
@@ -1,50 +1,58 @@
 'use strict';
 
-{
-  class Clock {
-    constructor() {
-      this.r = 100;
-    }
-
-    drawFace() {
-      const canvas = document.querySelector('canvas');
-      if (typeof canvas.getContext == 'undefined') {
-        return;
+(() => {
+  {
+    class ClockDrawer {
+      constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.width = canvas.width;
+        this.height = canvas.height;
       }
-      const ctx = canvas.getContext('2d');
+      
+    }
+    class Clock {
+      constructor(drawer) {
+        this.r = 100;
+        this.drawer = drawer;
+      }
 
-      const width = canvas.width;
-      const height = canvas.height;
+      drawFace() {
 
-      for (let angle = 0; angle < 360; angle += 6) {
-        ctx.save();
+        for (let angle = 0; angle < 360; angle += 6) {
+          ctx.save();
 
-        ctx.translate(width / 2, height / 2);
-        ctx.rotate(Math.PI / 180 * angle);
+          ctx.translate(width / 2, height / 2);
+          ctx.rotate(Math.PI / 180 * angle);
 
-        ctx.beginPath();
-        ctx.moveTo(0, -this.r);
-        if (angle % 30 == 0) {
-          ctx.lineWidth = 2;
-          ctx.lineTo(0, -this.r + 10);
-          ctx.font = '16px bold Arial';
-          ctx.textAlign = 'center';
-          ctx.fillText(angle / 30 || 12, 0, -this.r - 10);
-        } else {
+          ctx.beginPath();
+          ctx.moveTo(0, -this.r);
+          if (angle % 30 == 0) {
+            ctx.lineWidth = 2;
+            ctx.lineTo(0, -this.r + 10);
+            ctx.font = '16px bold Arial';
+            ctx.textAlign = 'center';
+            ctx.fillText(angle / 30 || 12, 0, -this.r - 10);
+          } else {
+            ctx.lineTo(0, -this.r + 5);
+          }
           ctx.lineTo(0, -this.r + 5);
-        }
-        ctx.lineTo(0, -this.r + 5);
-        ctx.stroke();
+          ctx.stroke();
 
-        ctx.restore();
+          ctx.restore();
+        }
+      }
+
+      run() {
+        this.drawFace();
       }
     }
 
-    run() {
-      this.drawFace();
+    const canvas = document.querySelector('canvas');
+    if (typeof canvas.getContext == 'undefined') {
+      return;
     }
-  }
 
-  const clock = new Clock();
-  clock.run();
-}
+    const clock = new Clock(new ClockDrawer(canvas));
+    clock.run();
+  }
+})();
