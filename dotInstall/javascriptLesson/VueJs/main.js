227276af0208ac3975bdde5b9d51f6c73be9b603
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-21 06:05:26 +0900

    [Edit]: Implemented UI in case of no ToDo using v-if
    Message body
    VueJs_9
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/VueJs/main.js
+++ dotInstall/javascriptLesson/VueJs/main.js
@@ -5,16 +5,17 @@
     el: '#app',
     data: {
       newItem: '',
-      todos: [{ 
-        title: 'task 1',
-        isDone: false
-      }, { 
-        title: 'task 2',
-        isDone: false
-      }, { 
-        title: 'task 3',
-        isDone:true 
-      },]
+      // todos: [{ 
+      //   title: 'task 1',
+      //   isDone: false
+      // }, { 
+      //   title: 'task 2',
+      //   isDone: false
+      // }, { 
+      //   title: 'task 3',
+      //   isDone:true 
+      // },]
+      todos: []
     },
     methods: {
       addItem: function(e) {
