b35678079ff0ea273ea98b195c5a680dfcc061cd
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-20 21:51:27 +0900

    [Edit]: Implemented process to add ToDo items
    Message body
    VueJs_5
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/VueJs/main.js
+++ dotInstall/javascriptLesson/VueJs/main.js
@@ -4,11 +4,18 @@
   var vm = new Vue({
     el: '#app',
     data: {
+      newItem: '',
       todos: [
         'task 1',
         'task 2',
         'task 3',
       ]
+    },
+    methods: {
+      addItem: function(e) {
+        this.todos.push(this.newItem);
+        this.newItem = '';
+      }
     }
   });
 })();
