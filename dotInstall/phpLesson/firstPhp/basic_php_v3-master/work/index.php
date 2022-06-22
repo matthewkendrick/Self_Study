1e5a29bb2431ce428014dfd8cb138a6bd8de1300
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-22 19:42:50 +0900

    [Edit]: Learn how to counter malicious code
    Message body
    FirstPhp_6
    
    Message footer
    Self-Study

--- dotInstall/phpLesson/firstPhp/basic_php_v3-master/work/index.php
+++ dotInstall/phpLesson/firstPhp/basic_php_v3-master/work/index.php
@@ -30,7 +30,7 @@ if ($n === 1) {
       </div>
       <div class="info">
         <h1>山田太郎</h1>
-        <p><?= $message; ?></p>
+        <p><?= htmlspecialchars($message, ENT_QUOTES, 'UTF-8'); ?></p>
         <ul>
           <li>
             <a href="https://dotinstall.com" target="_blank">
