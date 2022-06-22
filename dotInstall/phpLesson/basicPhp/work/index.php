2cdfc188ac2fac061d80b574d4df99363606ff48
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-23 08:10:14 +0900

    [Edit]: Know how to represent special characters
    Message body
    phpBasic_6
    
    Message footer
    Self-Study

--- dotInstall/phpLesson/basicPhp/work/index.php
+++ dotInstall/phpLesson/basicPhp/work/index.php
@@ -1,10 +1,6 @@
 <?php
 
-$name = 'Kendrick';
-$Name = 'Anna';
+$name = 'Anna';
 
-// echo 'Hello Anna' . PHP_EOL;
-// echo 'Hello Kendrick' . PHP_EOL;
-
-echo 'Hello '. $Name . PHP_EOL;
-echo 'Hi '. $name . PHP_EOL;
+echo "It's Sunday. Hi $name" . PHP_EOL;
+echo "It's \"Sunday\". Hi $name" . PHP_EOL;
