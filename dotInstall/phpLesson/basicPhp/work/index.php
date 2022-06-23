a47e1b660e92a8d81f41b0c2e84dde8b166d086c
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-24 06:14:15 +0900

    [Edit]: Added the ability to pass a null value
    Message body
    phpBasic_28
    
    Message footer
    Self-Study

--- dotInstall/phpLesson/basicPhp/work/index.php
+++ dotInstall/phpLesson/basicPhp/work/index.php
@@ -5,6 +5,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../css/stylesheet.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
   <title>Document</title>
 </head>
 <body>
@@ -15,14 +17,13 @@
 
           // declare(strict_types=1);
 
-          function showInfo(string $name, int $score) :void
+          function getAward(int $score): ?string
           {
-            echo $name . ': ' . $score . '<br>' . PHP_EOL;
+            return $score >= 100 ? '<span class="material-symbols-rounded">workspace_premium</span>' : null;
           }
 
-          showInfo('Anna', 50);
-          showInfo('Anna', '50');
-          // showInfo('Anna', 'text');
+          echo getAward(150) . '<br> Congratulations!!!' . PHP_EOL;
+          echo getAward(40) . '<br>' . PHP_EOL;
         ?>
       </p>
     </div>
