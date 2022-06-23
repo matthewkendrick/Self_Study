f7ae183165f1fbc91581bb1df9a14436232ce95e
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-23 22:06:32 +0900

    [Edit]: Learn about anonymous functions that treat functions as values
    Message body
    phpBasic_25
    
    Message footer
    Self-Study

--- dotInstall/phpLesson/basicPhp/work/index.php
+++ dotInstall/phpLesson/basicPhp/work/index.php
@@ -13,20 +13,12 @@
       <p>
         <?php
 
-        $rate = 1.1;
+        $sum = function ($a, $b, $c) {
+           return ( $a + $b + $c );
+         };
 
-        function sum($a, $b, $c) 
-         {
-           global $rate;
-          //  $rate = 1.08;
-           return ( $a + $b + $c ) * $rate;
-         }
+         echo $sum(100, 300, 500) . PHP_EOL;
 
-         echo sum(100, 200, 300);
-         echo '<br>';
-         echo sum(300, 400, 500);
-         echo '<br>';
-         echo sum(100, 200, 300) + sum(300, 400, 500);
         ?>
       </p>
     </div>
