1d32e9b34d19b74f94d4e0041b6c75165d37c6dc
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-23 15:14:41 +0900

    [Edit]: Know how to use the operators with substitutions
    Message body
    phpBasic_9
    
    Message footer
    Self-Study

--- dotInstall/phpLesson/basicPhp/work/index.php
+++ dotInstall/phpLesson/basicPhp/work/index.php
@@ -1,14 +1,27 @@
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="styles.css">
+  <link rel="stylesheet" href="../css/stylesheet.css">
   <title>Document</title>
 </head>
 <body>
-  <p>sample</p>
+  <section>
+    <div class="text">
+      <p>
+        <?php
+          $price = 500;
+          echo $price += 100;
+
+          $price *= 100;
+          echo $price++;
+        ?>
+      </p>
+    </div>
+  </section>
   
 </body>
 </html>
