9377d676d9c84a88ed81fe4c6bae869481e332f3
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-23 10:46:37 +0900

    [Edit]: CSS has applied
    Message body
    phpBasic_8
    
    Message footer
    Self-Study

--- dotInstall/phpLesson/basicPhp/work/index.php
+++ dotInstall/phpLesson/basicPhp/work/index.php
@@ -1,13 +1,14 @@
-<?php
-
-// echo 10 + 3 . PHP_EOL;
-// echo 10 - 3 . PHP_EOL;
-// echo 10 * 3 . PHP_EOL;
-// echo 10 / 3 . PHP_EOL;
-
-// echo 10 % 3 . PHP_EOL;
-// echo 10 ** 3 . PHP_EOL;
-
-echo ( 2 + 10 ) * 3 . PHP_EOL;
-
-echo 2 + '3' . PHP_EOL;
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>Document</title>
+</head>
+<body>
+  <p>sample</p>
+  
+</body>
+</html>
