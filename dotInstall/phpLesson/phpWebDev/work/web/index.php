95b05bded1e6bd06f345e4df4fa9d775a573acaa
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-28 20:57:20 +0900

    [Edit]: A different syntax was adopted for the control structure
    Message body
    webDev_7
    
    Message footer
    Self-Study

--- dotInstall/phpLesson/phpWebDev/work/web/index.php
+++ dotInstall/phpLesson/phpWebDev/work/web/index.php
@@ -3,7 +3,10 @@
   require('../app/functions.php');
 
   $today = date('Y-m-d H:i:s l');
-  $name = [
+  $names = [
+    'Anna',
+    'Matthew',
+    'Beth',
   ];
 
 ?>
@@ -17,15 +20,15 @@
 <body>
   <section>
     <div class="text">
-      <p><?= $today ?></p>
+      <p><?= $today ?></p><br>
       <ul class="">
-        <?php if (empty($names)) { ?>
+        <?php if (empty($names)): ?>
           <li>Nobody!</li>
-        <?php } else { ?>
-        <?php foreach ($names as $name) { ?>
-            <li><?= h($name) ?></li>
-        <?php } ?>
-        <?php } ?>
+        <?php else: ?>
+        <?php foreach ($names as $name): ?>
+            <li><?= h($name); ?></li>
+        <?php endforeach; ?>
+        <?php endif; ?>
       </ul>
     </div>
   </section>
