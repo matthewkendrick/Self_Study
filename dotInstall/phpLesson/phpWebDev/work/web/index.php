769a2b7c49683e6cc9ad801ebe88a9b9b895f437
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-28 19:04:33 +0900

    [Edit]: Tags or others can be converted to character entity references
    Message body
    webDev_4
    
    Message footer
    Self-Study

--- dotInstall/phpLesson/phpWebDev/work/web/index.php
+++ dotInstall/phpLesson/phpWebDev/work/web/index.php
@@ -1,6 +1,7 @@
 <?php
 
   $today = date('Y-m-d H:i:s l');
+  $name = 'Anna';
 
 ?>
 <!DOCTYPE html>
@@ -13,10 +14,7 @@
 <body>
   <section>
     <div class="text">
-      <p>Hello, PHP!</p>
-      <p>Today: <?php echo date('Y-m-d H:i:s l'); ?></p>
-      <p>Today: <?= date('Y-m-d H:i:s l'); ?></p>
-      <p>Today: <?= $today; ?></p>
+      <p>Hello, <?= htmlspecialchars($name, ENT_QUOTES, 'UTF-8'); ?></p>
     </div>
   </section>
 </body>
