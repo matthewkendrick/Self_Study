541c3d5f73de04745c51f756e84f039b057a0e68
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-06-28 22:45:26 +0900

    [Edit]: Componentization of code
    Message body
    webDev_8
    
    Message footer
    Self-Study

---
+++ dotInstall/phpLesson/phpWebDev/work/app/_parts/_header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="./css/styles.css">
+  <script src="https://kit.fontawesome.com/051eb814e7.js" crossorigin="anonymous"></script>
+  <title>PHP Practice</title>
+</head>
+<body>
--- dotInstall/phpLesson/phpWebDev/work/web/index.php
+++ dotInstall/phpLesson/phpWebDev/work/web/index.php
@@ -1,36 +1,18 @@
 <?php
 
   require('../app/functions.php');
+  include('../app/_parts/_header.php');
 
   $today = date('Y-m-d H:i:s l');
-  $names = [
-    'Anna',
-    'Matthew',
-    'Beth',
-  ];
 
 ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="utf-8">
-  <link rel="stylesheet" href="./css/styles.css">
-  <title>PHP Practice</title>
-</head>
-<body>
+
   <section>
     <div class="text">
       <p><?= $today ?></p><br>
-      <ul class="">
-        <?php if (empty($names)): ?>
-          <li>Nobody!</li>
-        <?php else: ?>
-        <?php foreach ($names as $name): ?>
-            <li><?= h($name); ?></li>
-        <?php endforeach; ?>
-        <?php endif; ?>
-      </ul>
     </div>
   </section>
-</body>
-</html>
+
+<?php 
+
+include('../app/_parts/_footer.php');
