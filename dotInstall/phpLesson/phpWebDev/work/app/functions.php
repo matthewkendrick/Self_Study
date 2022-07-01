be1ceaea10d14e9fb13e4158963851698c9d219d
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-02 06:05:25 +0900

    [FIX]: Created functions to generate and check tokens
    Message body
    webDev_31-33
    
    Message footer
    Self-Study

--- dotInstall/phpLesson/phpWebDev/work/app/functions.php
+++ dotInstall/phpLesson/phpWebDev/work/app/functions.php
@@ -6,4 +6,21 @@
   {
     return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
   }
-?>
+
+  function createToken()
+  {
+    if (!isset($_SESSION['token'])) {
+      $_SESSION['token'] = bin2hex( random_bytes(32) );
+    }
+  }
+
+  function validateToken()
+  {
+    if (
+      empty($_SESSION['token']) || $_SESSION['token'] !== filter_input(INPUT_POST, 'token')
+    ) {
+      exit('Invalid Post Request');
+    }
+  }
+
+  session_start;
