e94619a81d89f29e55be30baa30b4458b5d339e6
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-03 20:25:18 +0900

    [Edit]: Implemented click events
    Message body
    accordionUI_7
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/accordionUI/main.js
+++ dotInstall/javascriptLesson/accordionUI/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+{
+  const dts = document.querySelectorAll('dt');
+
+  dts.forEach(dt => {
+    dt.addEventListener('click', () => {
+      dt.parentNode.classList.toggle('appear');
+    });
+  });
+}
