b9274a3b08efabc2425cd74f85dfea4c60ce957b
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-03 22:50:07 +0900

    [Edit]: When one item is opened, the other items are closed
    Message body
    accordionUI_9
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/accordionUI/main.js
+++ dotInstall/javascriptLesson/accordionUI/main.js
@@ -6,6 +6,12 @@
   dts.forEach(dt => {
     dt.addEventListener('click', () => {
       dt.parentNode.classList.toggle('appear');
+
+      dts.forEach(el => {
+        if (dt != el) {
+          el.parentNode.classList.remove('appear');
+        }
+      });
     });
   });
 }
