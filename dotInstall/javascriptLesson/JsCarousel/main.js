678d457b6e83b62ace66ae4d0b9bbcf6b1b14fe9
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-04 20:34:50 +0900

    [Fix]: Fixed the image to slide sideways for one image
    Message body
    JsCarousel_9
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/JsCarousel/main.js
+++ dotInstall/javascriptLesson/JsCarousel/main.js
@@ -3,8 +3,10 @@
 {
   const next = document.getElementById('next');
   const ul = document.querySelector('ul');
+  const slides = ul.children;
 
   next.addEventListener('click', () => {
-    ul.style.transform = 'translateX(-200px)';
-  })
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    ul.style.transform = `translateX(${-1 * slideWidth}px)`;
+  });
 }
