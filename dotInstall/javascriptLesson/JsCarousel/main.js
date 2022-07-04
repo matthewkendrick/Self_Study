e486765af45a9486bf2002ce728596c797dd918b
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-04 20:39:03 +0900

    [Edit]: The distance traveled can be managed by a variable
    Message body
    JsCarousel_10
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/JsCarousel/main.js
+++ dotInstall/javascriptLesson/JsCarousel/main.js
@@ -4,9 +4,11 @@
   const next = document.getElementById('next');
   const ul = document.querySelector('ul');
   const slides = ul.children;
+  let currentIndex = 0;
 
   next.addEventListener('click', () => {
+    currentIndex++;
     const slideWidth = slides[0].getBoundingClientRect().width;
-    ul.style.transform = `translateX(${-1 * slideWidth}px)`;
+    ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
   });
 }
