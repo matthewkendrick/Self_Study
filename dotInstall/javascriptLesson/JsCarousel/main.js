2e844491457dd8c91a31059af8c8e9d98f671d4c
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-05 07:49:53 +0900

    [Edit]: Set the click event for the PREV button
    Message body
    JsCarousel_11-12
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/JsCarousel/main.js
+++ dotInstall/javascriptLesson/JsCarousel/main.js
@@ -2,12 +2,18 @@
 
 {
   const next = document.getElementById('next');
+  const prev = document.getElementById('prev');
   const ul = document.querySelector('ul');
   const slides = ul.children;
   let currentIndex = 0;
 
   next.addEventListener('click', () => {
     currentIndex++;
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
+  });
+  prev.addEventListener('click', () => {
+    currentIndex--;
     const slideWidth = slides[0].getBoundingClientRect().width;
     ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
   });
