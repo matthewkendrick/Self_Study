7bd70b0cdd22b20d18946d6140fb06eca9b269b8
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-06 02:17:49 +0900

    [Edit]: Tile state is kept in properties and can be drawn
    Message body
    15Puzzle_6
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/15Puzzle/main.js
+++ dotInstall/javascriptLesson/15Puzzle/main.js
@@ -5,22 +5,36 @@
     constructor(canvas) {
       this.canvas = canvas;
       this.ctx = this.canvas.getContext('2d');
+      this.tiles = [
+        [0, 1, 2, 3],
+        [4, 5, 6, 7],
+        [8, 9, 10, 11],
+        [12, 13, 14, 15],
+      ];
       this.img = document.createElement('img');
-      // this.renderTile(9, 2, 1);
-      this.renderTile(12, 3, 3);
+      this.img.src = 'img/15puzzle.png'
+      this.img.addEventListener('load', () => {
+        this.render();
+      });
+    }
+
+    render() {
+      for (let row = 0; row < 4; row++) {
+        for (let col = 0; col < 4; col++) {
+          this.renderTile(this.tiles[row][col], col, row);
+        }
+      }
     }
 
     renderTile(n, col, row) {
-      this.img.src = 'img/15puzzle.png';
-      this.img.addEventListener('load', () => {
-        this.ctx.drawImage(
-          this.img,
-          (n % 4) * 70, Math.floor(n / 4) * 70, 70, 70,
-          col * 70, row * 70, 70, 70,
-        );
-      });
+      this.ctx.drawImage(
+        this.img,
+        (n % 4) * 70, Math.floor(n / 4) * 70, 70, 70,
+        col * 70, row * 70, 70, 70,
+      );
     }
   }
+
   const canvas = document.querySelector('canvas');
   if (typeof canvas.getContext === 'undefined') {
     return;
