7c415bb0e586f175a8d2646377a9281ec3ad0f16
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-05 17:11:45 +0900

    Message footer Self-Study[Edit]: Created a Puzzle class
    Message body
    15Puzzle_3
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/15Puzzle/main.js
+++ dotInstall/javascriptLesson/15Puzzle/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-{}
+(() => {
+  class Puzzle {
+    constructor(canvas) {
+      this.canvas = canvas;
+      this.ctx = this.canvas.getContext('2d');
+    }
+    
+  }
+  const canvas = document.querySelector('canvas');
+  if (typeof canvas.getContext === 'undefined') {
+    return;
+  }
+  
+  new Puzzle(canvas);
+})();
