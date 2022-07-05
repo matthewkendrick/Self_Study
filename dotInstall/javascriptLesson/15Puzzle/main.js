7343803c483930c0663003cc1589a9af410af555
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-05 18:53:04 +0900

    [Edit]: The necessary information is given as an argument to drawImage() so that the panel can be cut out
    Message body
    15Puzzle_5
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/15Puzzle/main.js
+++ dotInstall/javascriptLesson/15Puzzle/main.js
@@ -6,7 +6,8 @@
       this.canvas = canvas;
       this.ctx = this.canvas.getContext('2d');
       this.img = document.createElement('img');
-      this.renderTile(9, 2, 1);
+      // this.renderTile(9, 2, 1);
+      this.renderTile(12, 3, 3);
     }
 
     renderTile(n, col, row) {
@@ -14,8 +15,8 @@
       this.img.addEventListener('load', () => {
         this.ctx.drawImage(
           this.img,
-          sx, sy, 70, 70,
-          dx, dy, 70, 70,
+          (n % 4) * 70, Math.floor(n / 4) * 70, 70, 70,
+          col * 70, row * 70, 70, 70,
         );
       });
     }
