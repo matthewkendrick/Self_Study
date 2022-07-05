3a29f7c80b4d46e6bdb59f4846f8df9bb86d131c
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-05 18:43:58 +0900

    [Edit]: Prepared to cut out the specified tile in the desired position
    Message body
    15Puzzle_4
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/15Puzzle/main.js
+++ dotInstall/javascriptLesson/15Puzzle/main.js
@@ -5,8 +5,20 @@
     constructor(canvas) {
       this.canvas = canvas;
       this.ctx = this.canvas.getContext('2d');
+      this.img = document.createElement('img');
+      this.renderTile(9, 2, 1);
     }
-    
+
+    renderTile(n, col, row) {
+      this.img.src = 'img/15puzzle.png';
+      this.img.addEventListener('load', () => {
+        this.ctx.drawImage(
+          this.img,
+          sx, sy, 70, 70,
+          dx, dy, 70, 70,
+        );
+      });
+    }
   }
   const canvas = document.querySelector('canvas');
   if (typeof canvas.getContext === 'undefined') {
