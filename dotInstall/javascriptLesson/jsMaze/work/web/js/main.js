6f38f8de9bade240bf3cab214678acae96d1bc75
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-08 04:07:15 +0900

    [Edit]: Retrieved the canvas element and prepared the Maze class
    Message body
    jsMaze_2
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/jsMaze/work/web/js/main.js
+++ dotInstall/javascriptLesson/jsMaze/work/web/js/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-{}
+(() => {
+  class Maze {
+    constructor() {
+      
+    }
+    
+  }
+  const canvas = document.querySelector('canvas');
+  if (typeof canvas.getContext === 'undefined') {
+    return;
+  }
+
+  const maze = new Maze(canvas);
+  maze.render();
+})();
