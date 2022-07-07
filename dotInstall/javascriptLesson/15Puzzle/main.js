2be02b693d1323685929aaf9a11d058c41c95c20
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-07 19:47:48 +0900

    [Edit]: I replaced the image
    Message body
    15Puzzle_23
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/15Puzzle/main.js
+++ dotInstall/javascriptLesson/15Puzzle/main.js
@@ -7,7 +7,7 @@
       this.canvas = canvas;
       this.ctx = this.canvas.getContext('2d');
       this.img = document.createElement('img');
-      this.img.src = 'img/15puzzle.png';
+      this.img.src = 'img/animal2.png';
       this.img.addEventListener('load', () => {
         this.render();
       });
@@ -43,15 +43,20 @@
       }
     }
     renderTile(n, col, row) {
-      this.ctx.drawImage(
-        this.img,
-        (n % 4) * 70, Math.floor(n / 4) * 70, 70, 70,
-        col * 70, row * 70, 70, 70
-      );
+      if (n === 15) {
+        this.ctx.fillStyle = '#fff';
+        this.ctx.fillRect(col * 70, row * 70, 70, 70);
+      } else {
+        this.ctx.drawImage(
+          this.img,
+          (n % 4) * 70, Math.floor(n / 4) * 70, 70, 70,
+          col * 70, row * 70, 70, 70
+        );
+      }
     }
   }
   class Puzzle {
-    constructor(canvas, level) {
+    constructor(level) {
       this.canvas = canvas;
       this.level = level;
       this.tiles = [
@@ -158,5 +163,5 @@
     return;
   }
 
-  new Puzzle(canvas, 10);
+  new PuzzleRenderer( new Puzzle(80), canvas );
 })();
