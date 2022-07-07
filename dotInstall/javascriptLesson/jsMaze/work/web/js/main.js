bb358e2a0d873885bc3344c2fbd89474bd9bee59
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-08 04:12:29 +0900

    [Edit]: Prepared array for the maze
    Message body
    jsMaze_3
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/jsMaze/work/web/js/main.js
+++ dotInstall/javascriptLesson/jsMaze/work/web/js/main.js
@@ -2,10 +2,24 @@
 
 (() => {
   class Maze {
-    constructor() {
-      
+    constructor(canvas) {
+      this.ctx = canvas.getContext('2d');
+      this.data = [
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1],
+        [1, 1, 1, 1, 1],
+      ];
     }
     
+    render() {
+      for (let row = 0; row < 5; row++) {
+        for (let col = 0; col < 5; col++) {
+          index
+        }
+      }
+    }
   }
   const canvas = document.querySelector('canvas');
   if (typeof canvas.getContext === 'undefined') {
