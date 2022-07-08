fb73e6876f6da1c2d4e899d47a550a06d95cdfd7
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-08 21:27:04 +0900

    [Edit]: Created Game class and prepared canvas
    Message body
    PinPon_3
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/PinPon/work/web/js/main.js
+++ dotInstall/javascriptLesson/PinPon/work/web/js/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-{}
+(() => {
+  class Game {
+    constructor(canvas) {
+      this.canvas = canvas;
+      this.ctx = this.canvas.getContext('2d');
+    }
+    
+  }
+  const canvas = document.querySelector('canvas');
+  if (typeof canvas.getContext === 'undefined') {
+    return;
+  }
+
+  new Game(canvas);
+})();
