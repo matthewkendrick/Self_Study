96dae3c37f991b9a15ade0dc9cf4063b86f73c99
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-08 21:48:00 +0900

    [Edit]: Ball class is defined and drawing process is implemented
    Message body
    PinPon_5
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/PinPon/work/web/js/main.js
+++ dotInstall/javascriptLesson/PinPon/work/web/js/main.js
@@ -1,10 +1,30 @@
 'use strict';
 
 (() => {
+  class Ball {
+    constructor(canvas) {
+      this.canvas = canvas;
+      this.ctx = this.canvas.getContext('2d');
+      this.x = 30;
+      this.y = 30;
+      this.r = 10;
+    }
+    
+    update() {
+    }
+    
+    draw() {
+      this.ctx.beginPath();
+      this.ctx.fillStyle = '#fdfdfd';
+      this.ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+      this.ctx.fill();
+    }
+  }
   class Game {
     constructor(canvas) {
       this.canvas = canvas;
       this.ctx = this.canvas.getContext('2d');
+      this.ball = new Ball(this.canvas);
       this.loop();
     }
 
@@ -19,10 +39,11 @@
     }
   
     update() {
+      this.ball.update();
     }  
 
     draw() {
-      console.log(new Date());
+      this.ball.draw();
     }
   }
   const canvas = document.querySelector('canvas');
