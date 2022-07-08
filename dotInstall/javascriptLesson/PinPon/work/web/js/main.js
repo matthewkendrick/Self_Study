f3794ff7fdcc33fac0572296013a3230795438ff
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-08 21:34:36 +0900

    3k[Edit]: The game loop was implemented
    Message body
    PinPon_4
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/PinPon/work/web/js/main.js
+++ dotInstall/javascriptLesson/PinPon/work/web/js/main.js
@@ -5,8 +5,25 @@
     constructor(canvas) {
       this.canvas = canvas;
       this.ctx = this.canvas.getContext('2d');
+      this.loop();
     }
+
     
+    loop() {
+      this.update();
+      this.draw();
+
+      requestAnimationFrame(() => {
+        this.loop();
+      });
+    }
+  
+    update() {
+    }  
+
+    draw() {
+      console.log(new Date());
+    }
   }
   const canvas = document.querySelector('canvas');
   if (typeof canvas.getContext === 'undefined') {
