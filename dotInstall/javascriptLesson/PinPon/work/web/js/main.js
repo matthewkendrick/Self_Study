10d5923c40e0167b9c44439fec23e29a9cda6073
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-10 08:06:50 +0900

    [Edit]: Prepared to displayed as screen as game over
    Message body
    PinPon_14
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/PinPon/work/web/js/main.js
+++ dotInstall/javascriptLesson/PinPon/work/web/js/main.js
@@ -120,9 +120,13 @@
       this.ball = new Ball(this.canvas);
       this.paddle = new Paddle(this.canvas);
       this.loop();
+      this.isGameOver = false;
     }
     
     loop() {
+      if (this.isGameOver) {
+        return;
+      }
       this.update();
       this.draw();
 
@@ -134,12 +138,26 @@
     update() {
       this.ball.update();
       this.paddle.update(this.ball);
+
+      if (this.ball.getMissedStatus()) {
+        this.isGameOver = true;
+      }
     }  
 
     draw() {
+      if (this.isGameOver) {
+        this.drawGameOver();
+        return;
+      }
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
       this.ball.draw();
       this.paddle.draw();
+    }
+
+    drawGameOver() {
+      this.ctx.font = '28px "Arial Black"';
+      this.ctx.fillStyle = 'tomato';
+      this.ctx.fillText = ('GAME OVER', 50, 150);
     }
   }
 
