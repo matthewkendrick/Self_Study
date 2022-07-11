6833b289cca808f4898048ae92b3571ae1d65212
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-11 23:04:38 +0900

    [Edit]: Implemented so that the "appear" class is given when it intersects the screen
    Message body
    jsApp_4
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/jsApp/work/web/js/main.js
+++ dotInstall/javascriptLesson/jsApp/work/web/js/main.js
@@ -1,8 +1,14 @@
 'use strict';
 
 {
-  function callback() {
-    
+  function callback(entries, obs) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+      entry.target.classList.add('appear');
+      obs.unobserve(entry.target);
+    });
   }
   const observer = new IntersectionObserver(callback, {
     threshold: 0.2,
