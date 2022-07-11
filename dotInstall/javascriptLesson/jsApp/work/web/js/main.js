6adb90c3201dd58523ac5b6b0e8581e664e08bb0
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-11 22:52:22 +0900

    [Edit]: Prepared to use the Intersection Observer API
    Message body
    jsApp_3
    
    Message footer
    Self-Study

--- dotInstall/javascriptLesson/jsApp/work/web/js/main.js
+++ dotInstall/javascriptLesson/jsApp/work/web/js/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-{}
+{
+  function callback() {
+    
+  }
+  const observer = new IntersectionObserver(callback, {
+    threshold: 0.2,
+  });
+
+  document.querySelectorAll('.animate').forEach(el => {
+    observer.observe(el);
+  });
+}
