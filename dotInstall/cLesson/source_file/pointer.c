cc45fb957561b47aa58f501e3727cdb25400c702
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-20 06:26:34 +0900

    [Edit]: Learned about memory savings by using pointers
    Message body
    cLesson_20
    
    Message footer
    Self-Study

--- dotInstall/cLesson/source_file/pointer.c
+++ dotInstall/cLesson/source_file/pointer.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+void f(long *pc) {
+  *pc += 100;
+  printf("%ld\n", *pc);
+}
+
 int main(void) {
   int a = 10;
   int b = 20;
@@ -12,6 +17,11 @@ int main(void) {
 
   printf("%d\n", *pa);
   printf("%d\n", *pb);
+
+  printf("----------\n");
+
+  long c = 1000;
+  f(&c);
 
   return 0;
 }
