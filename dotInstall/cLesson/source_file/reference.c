3ef71702add7ac4d46dcf11a0446db45ca930260
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-20 17:23:51 +0900

    [Edit]: Created swap function and checked its operation
    Message body
    cLesson_22
    
    Message footer
    Self-Study

--- dotInstall/cLesson/source_file/reference.c
+++ dotInstall/cLesson/source_file/reference.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 void swap(int *pa, int *pb) {
+  int tmp;
+  tmp = *pa;
+  *pa = *pb;
+  *pb = tmp;
 }
 
 int main() {
-  int a;
-  int b;
+  int a = 5;
+  int b = 10;
   swap(&a, &b);
   printf("a: %d, b: %d\n", a, b);
 
