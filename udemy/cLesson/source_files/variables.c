505e671da1761b09507460686e77660beb5fb633
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-21 18:16:42 +0900

    [Edit]: Learned about the basics of variables
    Message body
    cLesson_10_section4
    
    Message footer
    Self-Study

--- udemy/cLesson/source_files/variables.c
+++ udemy/cLesson/source_files/variables.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
+  int a;
+  int b = 3;
+  int add, sub;
+  double avg;
+  a = 10;
+  add = a + b;
+  sub = a - b;
+  avg = (a + b) / 2.0;
+
+  printf("%d + %d = %d\n", a, b, add);
+  printf("%d - %d = %d\n", a, b, sub);
+  printf("%dと%dの平均値: %f\n", a, b, avg);
 
   return 0;
 }
