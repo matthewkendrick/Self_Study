d2a55f4feff8e95d9b8f0c9cdb56ad129561c0af
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-21 18:02:59 +0900

    [Edit]: Learned the basics of arithmetic
    Message body
    cLesson_9_section4
    
    Message footer
    Self-Study

---
+++ udemy/cLesson/source_files/calcultion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  printf("%d + %d = %d\n", 5, 2, 5 + 2);
+  printf("%d - %d = %d\n", 5, 2, 5 - 2);
+  printf("%d * %d = %d\n", 5, 2, 5 * 2);
+  printf("%d / %d = %d 余り %d\n", 5, 2, 5 / 2 );
+
+  return 0;
+}
