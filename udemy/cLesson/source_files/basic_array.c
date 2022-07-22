d43a4acdf52307a298857f9e3a6116397d1d8111
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-22 16:30:38 +0900

    [Edit]: Learned about the basics of arrays
    Message body
    cLesson_27_section7
    
    Message footer
    Self-Study

--- udemy/cLesson/source_files/basic_array.c
+++ udemy/cLesson/source_files/basic_array.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
+  double d[4] = {1.2, 3.7, 4.1, 2.0};
+  double sum, avg;
+  int i;
+
+  sum = 0.0;
+  for (i = 0; i < 4; i++) {
+    printf("%f ", d[i]);
+    sum += d[i];
+  }
+
+  printf("\n");
+  
+  avg = sum / 4.0;
+  printf("合計値: %f\n", sum);
+  printf("平均値: %f\n", avg);
 
   return 0;
 }
