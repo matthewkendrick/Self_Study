674f1fd4faee99a46744e906d74b8925473c039b
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-23 19:46:25 +0900

    [Edit]: Learned about global and local variables
    Message body
    cLesson_35_section8
    
    Message footer
    Self-Study

--- udemy/cLesson/source_files/variables2.c
+++ udemy/cLesson/source_files/variables2.c
@@ -1,6 +1,38 @@
-#include <stdlib.h>
+#include <stdio.h>
 
-int main(void) {
+int global = 1000;
 
-  return 0;
+void func1(double, int);
+void func2();
+
+int main() {
+  double a = 123.41;
+  int b = 100;
+  global = 20;
+
+  printf("--------------------\n");
+  printf("main処理中...\n");
+  printf("\nglobal = %d\n", global);
+  printf("a = %f\nb = %d\n", a, b);
+  printf("--------------------\n");
+
+  func1(3.1, 4);
+  func2();
+}
+
+void func1(double a, int b) {
+  printf("func1処理中...\n");
+  printf("\nglobal = %d\n", global);
+  printf("a = %f, b = %d\n", a, b);
+  printf("--------------------\n");
+}
+
+void func2() {
+  double a = -4.1;
+  int b = 2;
+
+  printf("func2処理中...\n");
+  printf("\nglobal = %d\n", global);
+  printf("a = %f, b = %d\n", a, b);
+  printf("--------------------\n");
 }
