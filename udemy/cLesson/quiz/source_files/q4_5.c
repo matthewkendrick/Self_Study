c3a1fe5b6065a11f617f62b40c84732490353bcb
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-26 13:06:24 +0900

    [Edit]: Quiz 4-4~6 was conducted
    Message body
    cLesson_40_section10
    
    Message footer
    Self-Study

---
+++ udemy/cLesson/quiz/source_files/q4_5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  int a = 3;
+  int i = 0;
+
+  printf("\n==============================\n");
+  for (i = 0; i < a + 1; i++) {
+    printf("a = %d\n", i);
+  }
+  printf("\n==============================\n");
+}
