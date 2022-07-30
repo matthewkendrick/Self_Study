98ef247fda13217a23584acb89da799143aedc44
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-31 07:46:24 +0900

    [Edit]: Quiz 6-4 was conducted
    Message body
    cLesson_40_section10
    
    Message footer
    Self-Study

--- udemy/cLesson/quiz/source_files/q6_4.c
+++ udemy/cLesson/quiz/source_files/q6_4.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
-int main(void) {
+void numJudge(int);
 
-  return 0;
+int main(void) {
+  int a;
+
+  printf("==============================\n");
+  printf("整数値を入力してください → ");
+  scanf("%d", &a);
+  printf("\n");
+  numJudge(a);
+  printf("==============================\n");
+}
+
+void numJudge(int a) {
+  char str1[] = "入力された値は、";
+  char str2[] = "です。";
+  if (a == 0) {
+    printf("%s0 %s\n", str1, str2);
+  } else if (a % 2 == 0) {
+    printf("%s偶数%s\n", str1, str2);
+  } else {
+    printf("%s奇数%s\n", str1, str2);
+  }
 }
