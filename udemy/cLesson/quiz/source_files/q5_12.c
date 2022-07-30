8b1422d369d5371b44649a66187d66e20eb9dd4b
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-07-30 20:05:55 +0900

    [Edit]: Quiz 5-12 was conducted
    Message body
    cLesson_40_section10
    
    Message footer
    Self-Study

--- udemy/cLesson/quiz/source_files/q5_12.c
+++ udemy/cLesson/quiz/source_files/q5_12.c
@@ -3,6 +3,17 @@
 #include <time.h>
 
 int main(void) {
+  int array_size = 10, rand_max = 9, rand_min = 0;
+  int i, j, data[array_size][array_size];
+  srand((unsigned)time(NULL));
 
-  return 0;
+  printf("==============================\n");
+  for (i = 0; i < array_size; i++) {
+    for (j = 0; j < array_size; j++) {
+      data[i][j] = rand() % rand_max + rand_min;
+      printf("%d ", data[i][j]);
+    }
+    printf("\n");
+  }
+  printf("==============================\n");
 }
