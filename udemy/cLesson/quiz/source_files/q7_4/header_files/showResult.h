2732aaae01310162ff6234ed4d9fd65002d99e0c
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-08-04 05:39:45 +0900

    [Edit]: Quiz 7-4 was conducted, and Prepared for the Quiz 7-5
    Message body
    cLesson_40_section10
    
    Message footer
    Self-Study

---
+++ udemy/cLesson/quiz/source_files/q7_4/header_files/showResult.h
@@ -0,0 +1,6 @@
+#if !defined(_SHOWRESULT_H_)
+#define _SHOWRESULT_H_
+
+void showNum();
+
+#endif // _SHOWRESULT_H_
---
+++ udemy/cLesson/quiz/source_files/q7_4/source_files/calc.c
@@ -0,0 +1,16 @@
+#include "../header_files/calc.h"
+
+// 初期値の設定
+void init(int num) {
+  ans = num;
+}
+
+// 加算
+void add(int num) {
+  ans += num;
+}
+
+// 減算
+void sub(int num) {
+  ans -= num;
+}
