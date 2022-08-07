158961d28cc9963832ef2986132e1a2fe6e6fc83
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-08-07 16:14:45 +0900

    [Edit]: Quiz 7-5 was conducted
    Message body
    cLesson_40_section10
    
    Message footer
    Self-Study

--- udemy/cLesson/quiz/source_files/q7_5/header_files/showResult.h
+++ udemy/cLesson/quiz/source_files/q7_5/header_files/showResult.h
@@ -1,6 +1,7 @@
 #if !defined(_SHOWRESULT_H_)
 #define _SHOWRESULT_H_
 
-
+// 判定
+void Judge();
 
 #endif // _SHOWRESULT_H_
--- udemy/cLesson/quiz/source_files/q7_5/source_files/main.c
+++ udemy/cLesson/quiz/source_files/q7_5/source_files/main.c
@@ -1,2 +1,22 @@
+#include <stdio.h>
+#include "../header_files/logic.h"
+#include "../header_files/showResult.h"
+
 int main(void) {
+  int num;
+  init();
+
+  printf("じゃんけんゲーム\n");
+  printf("0：グー、1：チョキ、2：パー\n");
+  while (1) {
+    printf("あなたの手は？：");
+    scanf("%d", &num);
+    if (num < 0 || num > 2) {
+      printf("- 終了します -\n");
+    } else {
+      setPlayer(num);
+      setComputer();
+      Judge();
+    }
+  }
 }
