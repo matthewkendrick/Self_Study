14500a4a8c83eaa7a5e60cb17de873f8d266ec97
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-08-08 08:31:04 +0900

    [Edit]: Try to perform q6_6~7 file splitting
    Message body
    cLesson_40_section10
    
    Message footer
    Self-Study

---
+++ udemy/cLesson/quiz/source_files/q6_6/header_files/rep.h
@@ -0,0 +1,7 @@
+#if !defined(_REP_H_)
+#define _REP_H_
+#include <stdio.h>
+
+void stars(int);
+
+#endif // _REP_H_
---
+++ udemy/cLesson/quiz/source_files/q6_7/header_files/calc.h
@@ -0,0 +1,10 @@
+#if !defined(_CALC_H_)
+#define _CALC_H_
+#include <stdio.h>
+#include <math.h>
+
+double circumference(double);
+double area(double);
+double radius, result1, result2;
+
+#endif // _CALC_H_
