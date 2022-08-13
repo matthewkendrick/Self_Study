9293d8e7f1a2eb55ee6f7732a1e9f524cc1c778b
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-08-13 18:36:02 +0900

    [Edit]: Solved an questions that q8_3
    Message body
    cLesson_40_section10
    
    Message footer
    Self-Study

--- udemy/cLesson/quiz/source_files/quetion_08/q8_3.c
+++ udemy/cLesson/quiz/source_files/quetion_08/q8_3.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
+  int num, i;
+  int result = 1;
 
+  printf("==============================\n");
+  printf("数値を入力してください\n→ ");
+  scanf("%d", &num);
+  printf("\n");
+  for (i = 1; i <= num; i++) {
+    result = result * i;
+  }
+  printf("%dの階乗は、%dです。\n", num, result);
+  printf("==============================\n");
+
+  return 0;
 }
