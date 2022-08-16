c003a35fd98203dec6947a9484902069703d6e1a
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-08-17 08:08:20 +0900

    [Edit]: Solving an questions that q8_13
    Message body
    cLesson_40_section10
    
    Message footer
    Self-Study

--- udemy/cLesson/quiz/source_files/quetion_08/q8_13.c
+++ udemy/cLesson/quiz/source_files/quetion_08/q8_13.c
@@ -4,5 +4,20 @@
 
 int main(void) {
   int rand_max = 10, rand_min = 1;
+  int data_size = 10;
+  int data_1[data_size], data_2[data_size], data_3[data_size], data_4[data_size];
+  int i, j, val_1, val_2;
   srand((unsigned)time(NULL));
+
+  printf("n1");
+  for (i = 0; i < data_size; i++) {
+    data_1[i] = rand() % rand_max + rand_min;
+    printf("%d ", data_1[i]);
+  }
+
+  printf("n2");
+  for (i = 0; i < data_size; i++) {
+    data_2[i] = rand() % rand_max + rand_min;
+    printf("%d ", data_2[i]);
+  }
 }
