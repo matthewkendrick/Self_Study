b09c4f369af9d360e66061b937e7e4ec9e459c9a
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-08-19 06:53:54 +0900

    [Edit]: Learned about math other related functions
    Message body
    cLesson_42_section5
    
    Message footer
    Self-Study

--- udemy/cLesson/source_files/math_func2.c
+++ udemy/cLesson/source_files/math_func2.c
@@ -5,9 +5,15 @@
 
 int main(void) {
   srand((unsigned)time(NULL));
-  int rand_max = 20;
-  int n = rand() % (rand_max * 2) - rand_max;
-  double d1 = rand() % (double)((rand_max * 2) - rand_max);
-  printf("%d", n);
-  printf("%.2f", d1);
+  int randMax_intA = 50, n = rand() % (randMax_intA * 2) - randMax_intA;
+  int randMax_intB = 50, m = rand() % (randMax_intB * 2) - randMax_intB;
+  double randMax_doubleN = ((double)n * 2) / 10.0, randMax_doubleM = ((double)m * 2) / 10.0;
+  double d1 = randMax_doubleN, d2 = randMax_doubleM;
+
+  printf("==================================================\n");
+  printf("%d の絶対値は %d、\n", n, abs(n));
+  printf("%.1f の絶対値は %.1f、\n", d1, fabs(d1));
+  printf("%.1f の２乗は %.1f、\n", d2, pow(d2, 2));
+  printf("%.1f の平方根は %.1f です。\n", d2, sqrt(d2));
+  printf("==================================================\n");
 }
