23afce539c8d53dcab17e629010c7ae25cc14cfd
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-08-19 13:15:07 +0900

    [Edit]: Prepare for the next lesson that #45
    Message body
    cLesson_45_section11
    
    Message footer
    Self-Study

--- udemy/cLesson/source_files/bitwise.c
+++ udemy/cLesson/source_files/bitwise.c
@@ -3,4 +3,12 @@
 int main(void) {
   unsigned char i = 0xf;
   unsigned char j = 0xff;
+
+  printf("====================\n");
+  printf("%x << 1 = 0x%x\n", i, i << 1);
+  printf("%x >> 1 = 0x%x\n", i, i >> 1);
+  printf("%x | %x = 0x%x\n", i, j, i | 1);
+  printf("%x & %x = 0x%x\n", i, j, i & 1);
+  printf("~%x = 0x%x\n", i, (unsigned char) ~i);
+  printf("====================\n");
 }
