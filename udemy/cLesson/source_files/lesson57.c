571dfba1c299ed36a7c94670899491572f64259f
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-08-20 20:04:16 +0900

    [New]: Prepare for the next lesson that #57
    Message body
    cLesson_57_section13
    
    Message footer
    Self-Study

---
+++ udemy/cLesson/source_files/lesson57.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 256
+
+int main(void) {
+  srand((unsigned)time(NULL));
+  int rand_max = 1000, rand_min = 100;
+  char s1[SIZE], s2[SIZE];
+  int a = rand() % (rand_max - rand_min) + rand_min, b = rand() % (rand_max - rand_min) + rand_min;
+
+  printf("====================");
+  sprintf(s1, "a の値は %d 、", a);
+  sprintf(s2, "b の値は %d です。", b);
+  printf("\n");
+  puts(s1);
+  puts(s2);
+  printf("====================\n");
+}
