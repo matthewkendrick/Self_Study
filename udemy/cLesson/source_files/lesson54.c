2cd285182b7435c8f1975bad0626229d94f489f5
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-08-20 17:11:50 +0900

    [Edit]: Learned about string functions
    Message body
    cLesson_54_section13
    
    Message footer
    Self-Study

--- udemy/cLesson/source_files/lesson54.c
+++ udemy/cLesson/source_files/lesson54.c
@@ -2,6 +2,27 @@
 #include <string.h>
 
 int main(void) {
-  char s[10];
+  char s[20];
   int len;
+
+  printf("==============================\n");
+  // ここでは文字列をコピーしています。
+  strcpy(s, "ABCD");
+  printf("s = %s", s);
+  printf("\n");
+
+  len = strlen(s);;
+  printf("文字列の長さ：%d", len);
+  printf("\n");
+  printf("------------------------------\n");
+
+  // ここでは上記でコピーした文字列を結合しています。
+  strcat(s, "EFGH");
+  printf("s = %s", s);
+  printf("\n");
+
+  len = strlen(s);;
+  printf("文字列の長さ：%d", len);
+  printf("\n");
+  printf("==============================\n");
 }
