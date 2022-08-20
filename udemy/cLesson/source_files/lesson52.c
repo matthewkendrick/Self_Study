caf35431efccadf70a3d0622fb1468f66e022210
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-08-20 12:01:43 +0900

    [Edit]: Learned about memory creation and deletion
    Message body
    cLesson_52_section12
    
    Message footer
    Self-Study

--- udemy/cLesson/source_files/lesson52.c
+++ udemy/cLesson/source_files/lesson52.c
@@ -3,4 +3,24 @@
 #define SIZE 3
 
 int main(void) {
+  int* p1 = NULL;
+  double *p2 = NULL;
+  int i;
+
+  p1 = (int*)malloc(sizeof(int)*SIZE);
+  p2 = (double*)malloc(sizeof(double)*SIZE);
+
+  printf("==============================\n");
+  for (i = 0; i < SIZE; i++) {
+    p1[i] = i;
+    p2[i] = i / 10.0;
+  }
+
+  for (i = 0; i < SIZE; i++) {
+    printf("p1[%d] = %d | p2[%d] = %.2f\n", i, p1[i], i, p2[i]);
+  }
+  printf("==============================\n");
+
+  free(p1);
+  free(p2);
 }
