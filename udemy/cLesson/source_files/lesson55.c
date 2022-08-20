4b98be5258892176ee69bb99db832a7ba828bf24
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-08-20 19:32:48 +0900

    [Edit]: Learned about supplemental string functions
    Message body
    cLesson_55_section13
    
    Message footer
    Self-Study

--- udemy/cLesson/source_files/lesson55.c
+++ udemy/cLesson/source_files/lesson55.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+#define SIZE 256
 
 int main(void) {
+  char s1[SIZE], s2[SIZE];
+
+  printf("========================================\n");
+  printf("s1 = ");
+  scanf("%s", s1);
+
+  printf("s2 = ");
+  scanf("%s", s2);
+
+  printf("\n");
+  if (strcmp(s1, s2) == 0) {
+    printf("s1 と s2 は等しいです。");
+  } else {
+    printf("s1 と s2 は等しくありません。");
+  }
+  printf("\n");
+  printf("========================================\n");
 }
