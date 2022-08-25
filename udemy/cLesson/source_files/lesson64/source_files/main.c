3b17b8d48315a38fa973b2aa320ea254f2a177ea
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-08-25 15:30:15 +0900

    [New]: Prepare for the next lesson that #64
    Message body
    cLesson_64_section15
    
    Message footer
    Self-Study

---
+++ udemy/cLesson/source_files/lesson64/source_files/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  FILE *file;
+  file = fopen(c:\\test\\sample.txt, "w");
+  if (file == NULL) {
+    printf("ファイルが開けません。\n");
+    exit(1);
+  }
+
+  fprintf(file, "Hello World.\n\n");
+  fprintf(file, "ABCDEF\n\n");
+  fclose(file);
+}
