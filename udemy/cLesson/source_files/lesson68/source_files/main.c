078c6b9eaeab2daf955bb6733f0cc4036804a9b3
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-08-31 21:25:03 +0900

    [New]: Prepare for the next lesson that #68
    Message body
    cLesson_67_section15
    
    Message footer
    Self-Study

---
+++ udemy/cLesson/source_files/lesson68/source_files/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  FILE* file;
+  int i, size;
+  char* rdata;
+  file = fopen("/Users/matthewkendrick/Desktop/Self_Study/udemy/cLesson/source_files/lesson68/test/test.bin", "rb");
+    if (file == NULL) {
+      printf("ファイルが開けません。\n");
+      exit(1);
+    }
+    fseek(file, 0, SEEK_END);
+    size = ftell(file);
+    rdata = (char*)malloc(sizeof(char) *size);
+
+    fseek(file, 0, SEEK_SET);
+    fread(rdata, sizeof(char), size, file);
+  fclose(file);
+
+  printf("====================\n");
+  for (i = 0; i < size; i++) {
+    printf("%x ", rdata[i]);
+  }
+  printf("\n====================\n");
+  free(rdata);
+}
