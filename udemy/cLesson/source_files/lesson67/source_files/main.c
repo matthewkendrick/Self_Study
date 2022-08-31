2370e1e5fe45ccb7ad4ee1df02b7f43fe2e67248
matthewkendrick <75340042+matthewkendrick@users.noreply.github.com>
2022-08-31 11:18:52 +0900

    [Edit]: Learned to read and write binary files
    Message body
    cLesson_67_section15
    
    Message footer
    Self-Study

---
+++ udemy/cLesson/source_files/lesson67/source_files/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  FILE *file;
+  int i;
+
+  char wdata[] = {0x10, 0x1e, 0x1e, 0x1f};
+  char rdata[4];
+
+  file = fopen("FilePath", "wb");
+    if (file == NULL) {
+      printf("ファイルが開けません。\n");
+      exit(1);
+    }
+    fwrite(wdata, sizeof(char), sizeof(wdata), file);
+  fclose(file);
+
+  file = fopen("FilePath", "rb");
+    if (file == NULL) {
+      printf("ファイルが開けません。\n");
+      exit(1);
+    }
+    fread(rdata, sizeof(char), sizeof(rdata), file);
+  fclose(file);
+  printf("=======\n");
+  for (i = 0; i < sizeof(rdata); i++) {
+    printf("%x \n", rdata[i]);
+  }
+  printf("=======\n");
+}
